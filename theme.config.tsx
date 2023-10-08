714e07ebd74b052afecb61d1eebd4e6205677f08
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-07 22:53:43 -0500

    theme and ts config

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Embedding Playbook</span>,
+  project: {
+    link: 'https://github.com/Tab-SE/playbook_frontend',
+  },
+  docsRepositoryBase: 'https://github.com/Tab-SE/playbook_frontend',
+  footer: {
+    text: 'Tableau Embedding Playbook',
+  },
+}
+
+export default config
