7c7e72670638874048f37666766e699ee5605422
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-08 16:23:06 -0500

    removing function

--- next.config.js
+++ next.config.js
@@ -3,25 +3,21 @@ let assetPrefix;
 // used by <Image/> component
 let basePath;
 
-const pathFinder = () => {
-  // sets paths for different export environments
-
-  // exports via GitHub Actions  
-  // https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
-  const isGithubActions = process.env.GITHUB_ACTIONS || false;
-  if (isGithubActions) {
-    // trim off `<owner>/`
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-  } 
-  // exporting to GitHub Pages without using Actions via package.json scripts
-  else if (process.env.EXPORT_STRATEGY === 'manual') {
-    const repo = process.env.GITHUB_REPOSITORY;
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-  }
-};
+// exports via GitHub Actions  
+// https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+} 
+// exporting to GitHub Pages without using Actions via package.json scripts
+else if (process.env.EXPORT_STRATEGY === 'manual') {
+  const repo = process.env.GITHUB_REPOSITORY;
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 // initializes the withNextra config object with theme settings
 const withNextra = require('nextra')({
