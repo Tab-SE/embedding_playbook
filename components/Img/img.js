92f1286ed285e915cc5ed9ac841c1a24d62d64b0
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-07 22:42:59 -0500

    tableau and image components

---
+++ components/Img/img.js
@@ -0,0 +1,26 @@
+// higher-order component abstracting the IMGX custom loader
+import Image from 'next/image'
+
+function imageLoader({ src, width, quality }) {
+  const url = new URL(`https://embeddingplaybook.imgix.net/${src}`);
+  const params = url.searchParams;
+  params.set('auto', params.getAll('auto').join(',') || 'format');
+  params.set('fit', params.get('fit') || 'max');
+  params.set('w', params.get('w') || width.toString());
+  params.set('q', (quality || 50).toString());
+  return url.href;
+}
+ 
+function Img(props) {
+  return (
+    <Image 
+      loader={imageLoader}
+      alt={props.alt} 
+      src={props.src} 
+      width={props.width}
+      height={props.height}
+    />
+  )
+}
+
+export default Img;
---
+++ components/Tableau/api/embed_api.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+const tab_embed = typeof window !== 'undefined' ? require("../api/public.tableau.com_javascripts_api_tableau.embedding.3.latest.min.js") : null;
+
+// import tab_embed from './../api/public.tableau.com_javascripts_api_tableau.embedding.3.latest.min.js';
+
+export default tab_embed;
