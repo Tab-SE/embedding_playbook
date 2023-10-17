ad3cfc28d428210e601e1c587bddf4ef633f8d60
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-16 19:03:51 -0500

    adding a blur placeholder

--- components/Img/img.js
+++ components/Img/img.js
@@ -1,5 +1,7 @@
 // higher-order component abstracting the IMGX custom loader
 import Image from 'next/image'
+import { useConfig } from 'nextra-theme-docs'
+import { useState, useEffect } from 'react';
 
 function imageLoader({ src, width, quality }) {
   const url = new URL(`https://tableauembeddingplaybook.imgix.net/${src}`);
@@ -12,13 +14,34 @@ function imageLoader({ src, width, quality }) {
 }
  
 function Img(props) {
+  // default theme is light, default base64 placeholder image is transparent black https://png-pixel.com/
+  const [blurDataURL, setBlurDataURL] = useState('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkmAkAAJ8AmzsOXcYAAAAASUVORK5CYII=');
+
+  const { darkMode } = useConfig();
+
+  useEffect(() => {
+    handleBlurDataURL();
+  }, []);
+  
+  const handleBlurDataURL = () => {
+    if (darkMode) {
+      // https://png-pixel.com/
+      setBlurDataURL('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8PxMAAp0BmiC7I60AAAAASUVORK5CYII=');
+    } else {
+      setBlurDataURL('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkmAkAAJ8AmzsOXcYAAAAASUVORK5CYII=');
+    }
+  }
+
   return (
     <Image 
-      loader={imageLoader}
       alt={props.alt} 
       src={props.src} 
       width={props.width}
       height={props.height}
+      loader={imageLoader}
+      sizes="(min-width: 320px) 25vw, 50vw, 75vw, 100vw"
+      placeholder="blur"
+      blurDataURL={`data:image/png;base64,${blurDataURL}`}
     />
   )
 }
