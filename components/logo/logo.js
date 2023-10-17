efa8f2d4a45dd91266b1eb4f5ad6d288b8ebc91a
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-16 22:14:40 -0500

    simplifying component

--- components/logo/logo.js
+++ components/logo/logo.js
@@ -1,17 +1,7 @@
-import { useState, useEffect } from 'react';
-import { useConfig } from 'nextra-theme-docs'
 import Img from '../img/img'
 
 function Logo() {
-  const [logo, setLogo] = useState('');
-  const { darkMode } = useConfig();
 
-  const handleSetLogo = () => {
-    if (darkMode) {
-    } else {
-    }
-  }
-  
   return (
   <>
   <span className='mr-2'>
