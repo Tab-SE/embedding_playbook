3d40bd76cbcd33508c5f3db38cba5faad1b3f739
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-18 23:02:16 -0500

    cleaned up custom head markup

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from 'next/router'
 import { useConfig, DocsThemeConfig } from 'nextra-theme-docs';
 import Logo from './components/logo/logo';
+import Head from './components/head/head'
 
 const description = "This playbook teaches you how to compose Tableau's varied product capabilities into applications that thrill customers, coworkers and friends!";
 
 const config: DocsThemeConfig = {
   logo: Logo,
   project: {
-    link: 'https://tab-se.github.io/embedding_playbook',
+    link: 'https://github.com/Tab-SE/embedding_playbook',
   },
   docsRepositoryBase: 'https://github.com/Tab-SE/embedding_playbook/tree/main/',
   footer: {
@@ -27,45 +28,22 @@ const config: DocsThemeConfig = {
     const imgix = 'https://tableauembeddingplaybook.imgix.net/'
     const socialCard =
       route === '/' || !title
-        ? imgix + 'tableau/logo_text.png'
+        ? imgix + 'tableau/logo_text.png?w=350'
         : `https://tab-se.github.io/api/og?title=${title}`;
 
     return (
       <>
-        <meta name="msapplication-TileColor" content="#fff" />
-        <meta name="theme-color" content="#fff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Language" content="en" />
-        <meta
-          name="description"
-          content={description}
+        <Head
+          title={title}
+          description={description}
+          imgix={imgix}
+          socialCard={socialCard}
         />
-        <meta
-          name="og:description"
-          content={description}
-        />
-        <meta name="twitter:card" content={imgix + "tableau/logo_text.png"} />
-        <meta name="twitter:image" content={socialCard} />
-        <meta name="twitter:site:domain" content="https://tab-se.github.io" />
-        <meta name="twitter:url" content="https://tab-se.github.io/embedding_playbook" />
-        <meta name="og:title" content={title}/>
-        <meta name="og:image" content={socialCard} />
-        <meta name="apple-mobile-web-app-title" content={title} />
-        <link rel="icon" href={imgix + "svg/logo_color.svg"} type="image/svg+xml" />
-        <link rel="icon" href={imgix + "tableau/tableau.ico"} type="image/ico" />
-        <link rel="icon" href={imgix + "tableau/tableau_logo.png"} type="image/png" />
-        <link
-          rel="icon"
-          href={imgix + "svg/dark.svg"}
-          type="image/svg+xml"
-          media="(prefers-color-scheme: dark)"
-        />
-        <link
-          rel="icon"
-          href={imgix + "tableau/tableau_logo_dark.png"}
-          type="image/png"
-          media="(prefers-color-scheme: dark)"
-        />
+        <link rel="icon" href={imgix + "svg/logo_color.svg?h=32&w=32"} type="image/svg+xml" />
+        <link rel="icon" href={imgix + "tableau/tableau.ico?h=32&w=32"} type="image/ico" />
+        <link rel="icon" href={imgix + "tableau/tableau_logo.png?h=32&w=32"} type="image/png" />
+        <link rel="icon" href={imgix + "svg/dark.svg?h=32&w=32"} type="image/svg+xml" media="(prefers-color-scheme: dark)" />
+        <link rel="icon" href={imgix + "tableau/tableau_logo_dark.png?h=32&w=32"} type="image/png" media="(prefers-color-scheme: dark)" />
       </>
     )
   }
