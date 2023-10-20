5915b5e27a092bd65dfbf6828a1d419257c600f4
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-20 00:24:56 -0500

    created a filter button

---
+++ components/toolbar/components/filter.js
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilter } from '@fortawesome/free-solid-svg-icons'
+
+function Menu(props) {
+  return (
+    <div className="card-body">
+      <span className="font-bold text-lg">6 Items</span>
+      <span className="text-info">Subtotal: $712</span>
+      <div className="card-actions">
+        <button className="btn btn-primary btn-block">View cart</button>
+      </div>
+    </div>
+  )
+}
+
+function Download(props) {
+  return (
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className='btn bg-accent btn-circle border-none'>
+        <div className="indicator">
+          <FontAwesomeIcon icon={faFilter} className='text-white' size='2xl' />
+        </div>
+      </label>
+      <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+        <Menu />
+      </div>
+    </div>
+  )
+}
+
+export default Download;
