2fc94a48811e770186188dde678d1f1adad1962d
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-19 22:26:40 -0500

    fixed relative path

--- components/Tableau/api/embed_api.js
+++ components/Tableau/api/embed_api.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-const tab_embed = typeof window !== 'undefined' ? require("../api/public.tableau.com_javascripts_api_tableau.embedding.3.latest.min.js") : null;
+const tab_embed = typeof window !== 'undefined' ? require("./public.tableau.com_javascripts_api_tableau.embedding.3.latest.min.js") : null;
 
 // import tab_embed from './../api/public.tableau.com_javascripts_api_tableau.embedding.3.latest.min.js';
 
