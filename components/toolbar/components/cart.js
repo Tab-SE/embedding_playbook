bb4783b24a8b2031bedb20faa999cc5c55a10a36
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-19 23:09:27 -0500

    sample UI button

--- components/toolbar/components/cart.js
+++ components/toolbar/components/cart.js
@@ -2,7 +2,7 @@ function Toggle() {
   return (
     <>
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-      <span className="badge badge-secondary badge-sm indicator-item">9</span>
+      <span className="badge badge-secondary badge-sm indicator-item">6</span>
     </>
   )
 }
@@ -11,7 +11,7 @@ function Menu() {
   return (
     <div className="card-body">
       <span className="font-bold text-lg">6 Items</span>
-      <span className="text-info">Subtotal: $712</span>
+      <span className="text-info">Subtotal: $712.40</span>
       <div className="card-actions">
         <button className="btn btn-primary btn-block">View cart</button>
       </div>
