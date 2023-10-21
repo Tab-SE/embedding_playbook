50a3a1311368c9b5cc71d05e6a1122bfb7ac6dea
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-21 18:29:23 -0500

    exchanging viz object from Tableau to Toolbar

--- components/analytics/analytics.js
+++ components/analytics/analytics.js
@@ -1,12 +1,16 @@
+import { useState, useEffect } from 'react'
 import Tableau from '../tableau/tableau'
 import Toolbar from '../toolbar/toolbar'
 
 function Analytics(props) {
+  // can lift the viz object and share it with other UI components
+  const [viz, setViz] = useState(undefined);
 
   return (
     <section className='bg-sfneutral80 dark:bg-sfneutral95 rounded'> 
-      <Toolbar/>
+      <Toolbar viz={viz} />
       <Tableau
+        setVizLift={setViz}
         vizUrl={props.vizUrl}
         height={props.height}
         width={props.width}
