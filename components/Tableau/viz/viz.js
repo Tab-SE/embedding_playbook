d34fd3384541a01a6eb48bbbffd4417a469ad003
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-22 14:04:20 -0500

    initializes tabScale first to handle scaling before interactivity

--- components/Tableau/viz/viz.js
+++ components/Tableau/viz/viz.js
@@ -1,23 +1,25 @@
-import { useEffect, useState, useRef } from 'react'
 // eslint-disable-next-line no-unused-vars
 import tab_embed from '../../embed_api/embed_api'
+import { useEffect, useState, useRef } from 'react'
+import TabScale from 'tabscale';
+
 
 function Viz(props) {
   const vizRef = useRef(null); // useRef accesses DOM nodes created with the render method https://reactjs.org/docs/refs-and-the-dom.html
   const [isClient, setIsClient] = useState(false); // can elevate load state to other components
   const [jwt, setJWT] = useState(''); // not yet in use, just referenced
+  let tabScale;
 
   useEffect(() => {
     handleIsClient();
-    handleVizObj();
-    handleFirstInteractive();
   }, [props.vizObj, isClient]);
 
 
   const handleIsClient = () => {
     // sets isClient to true once per component mount
-    // sets it to isClient upon unmount with return statement
     setIsClient(true);
+    handleVizObj();
+    // sets it to isClient upon unmount with return statement
     return () => {
       setIsClient(false);
     };
@@ -25,20 +27,26 @@ function Viz(props) {
 
   const handleVizObj = () => {
     // if the component has mounted and there is a valid viz object
-  // then set vizRef to the viz object. State is  nullified on unmount
+    // then set vizRef to the viz object. State is  nullified on unmount
     if (isClient && !props.vizObj) {
+      // passing the viz DOM element to tabScale https://gitlab.com/jhegele/tabscale
+      tabScale = new TabScale.Scale(vizRef.current);
       props.setVizObj(vizRef.current);
+      handleFirstInteractive();
     }
     return () => {
       if (props.vizObj) {
+        // when component unmounts reset viz objectss
         props.setVizObj(null);
+        tabScale = undefined;
       }
     };
   };
 
   const handleFirstInteractive = () => {
-    if (props.vizObj) { // if state has an initialized Tableau viz
+    if (props.vizObj && isClient) { // if state has an initialized Tableau viz
       props.vizObj.addEventListener('firstinteractive', async (event) => { // add the custom event listener to <tableau-viz>
+        tabScale.initialize(); // initializing tabScale
         props.setInteractive(true); // update state to indicate that the Tableau viz is interactive
       });
 
