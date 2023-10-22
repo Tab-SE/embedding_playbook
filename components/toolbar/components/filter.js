6df8148eb45340c92314c501e0688c9c7d6eb74b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-22 16:37:40 -0500

    filter modal displaying

--- components/toolbar/components/filter.js
+++ components/toolbar/components/filter.js
@@ -1,30 +1,38 @@
+import { useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilter } from '@fortawesome/free-solid-svg-icons'
 
-function Menu(props) {
+function FiltersBtn(props) {
   return (
-    <div className="card-body">
-      <span className="font-bold text-lg">6 Items</span>
-      <span className="text-info">Subtotal: $712</span>
-      <div className="card-actions">
-        <button className="btn btn-primary btn-block">View cart</button>
-      </div>
-    </div>
+    <button 
+    className="btn bg-sf-indigo btn-circle border-none outline-none text-white hover:text-sf-indigo" 
+    onClick={()=>props.modal.showModal()}>
+      <FontAwesomeIcon icon={faFilter} size='2xl' />
+    </button>
   )
 }
 
 function Download(props) {
+  // useRef accesses DOM nodes created with the render method https://reactjs.org/docs/refs-and-the-dom.html
+  const ref = useRef(null); 
+  const modal = ref.current;
+
   return (
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className='btn bg-sf-indigo btn-circle border-none text-white hover:text-sf-indigo'>
-        <div className="indicator">
-          <FontAwesomeIcon icon={faFilter} size='2xl' />
+    <>
+      <FiltersBtn modal={modal} />
+      <dialog id="my_modal_2" className="modal" ref={ref}>
+        <div className="modal-box">
+          <h3 className="font-bold text-2xl">
+            <FontAwesomeIcon icon={faFilter} size='lg' />
+            <span className='m-4'>Filters</span>
+          </h3>
+          <p className="py-4">Press ESC key or click outside to close</p>
         </div>
-      </label>
-      <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-        <Menu />
-      </div>
-    </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   )
 }
 
