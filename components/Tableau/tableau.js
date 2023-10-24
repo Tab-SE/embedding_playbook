04089932998cc7e5b2f53d6acb4a49cb86aaf07b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-10-24 11:54:04 -0500

    added support for parent and local viz objects

--- components/Tableau/tableau.js
+++ components/Tableau/tableau.js
@@ -3,25 +3,17 @@ import Viz from './viz/viz'
 
 // higher-order component composing multiple components into a single <Tableau/> component
 function Tableau(props) {
-  // viz object created here shared with child viz and parents via setVizLift()
-  const [vizObj, setVizObj] = useState(undefined); // "viz object" providing access to Tableau API methods
-  const [interactive, setInteractive] = useState(false); // viz interactivity state
-  const hideTabs = props.hideTabs === 'true' ? true : false; // converts to boolean saving default to false
+  const [localVizObj, setLocalVizObj] = useState(null);
+  const [localInteractive, setLocalInteractive] = useState(false);
 
-  // parent component must provide function handlers for props in this hook:
-  useEffect(() => {
-    if (props.setVizLift) {
-      props.setVizLift(vizObj); // lifts the viz object to parent nodes
-      props.setInteractiveLift(interactive); // lifts interactive state to parent nodes
-    }
-  },[vizObj, interactive, props]);
+  const hideTabs = props.hideTabs === 'true' ? true : false; // converts to boolean saving default to false
 
   return (
     <Viz
-      vizObj={vizObj}
-      setVizObj={setVizObj}
-      interactive={interactive}
-      setInteractive={setInteractive}
+      vizObj={props.vizObj ? props.vizObj : localVizObj}
+      setVizObj={props.setVizObj ? props.setVizObj : setLocalVizObj}
+      interactive={props.interactive ? props.interactive : localInteractive}
+      setInteractive={props.setInteractive ? props.setInteractive : setLocalInteractive}
       vizUrl={props.vizUrl}
       height={props.height}
       width={props.width}
--- components/analytics/analytics.js
+++ components/analytics/analytics.js
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Tableau from '../tableau/tableau'
 import Toolbar from '../toolbar/toolbar'
 
 function Analytics(props) {
   // lifts vizObj and shares it with other UI components
-  const [viz, setViz] = useState(undefined);
+  const [vizObj, setVizObj] = useState(null);
   const [interactive, setInteractive] = useState(false);
+
+  useEffect(() => {
+    // console.log('analytics', vizObj);
+  }, [vizObj])
 
   return (
     <section className='bg-colorblind-lgray rounded'> 
-      <Toolbar viz={viz} interactive={interactive} />
+      <Toolbar viz={vizObj} interactive={interactive} />
       <div className='grid place-items-center'>
         <Tableau
-          setVizLift={setViz}
-          setInteractiveLift={setInteractive}
+          vizObj={vizObj}
+          setVizObj={setVizObj}
+          interactive={interactive}
+          setInteractive={setInteractive}
           vizUrl={props.vizUrl}
           height={props.height}
           width={props.width}
