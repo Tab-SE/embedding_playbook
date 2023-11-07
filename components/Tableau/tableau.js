1816ba7c4433fe12f7a6da8b83a90324c823607c
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-11-06 22:31:57 -0600

    baseline MVC, separating model, view and controller code

--- components/Tableau/tableau.js
+++ components/Tableau/tableau.js
@@ -1,17 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useState, useMemo } from 'react'
 import Viz from './components/viz'
+import Obj from './components/obj'
 
 // higher-order component composing multiple components into a single <Tableau/> component
 function Tableau(props) {
-  const [localVizObj, setLocalVizObj] = useState(null);
+  // const [localVizObj, setLocalVizObj] = useState(null);
   const [localInteractive, setLocalInteractive] = useState(false);
+  const vizObj = new Obj();
+
+  const setVizObj = (viz) => {
+    vizObj = useMemo(() => {
+
+    },[viz]);
+  };
 
   const hideTabs = props.hideTabs === 'true' ? true : false; // converts to boolean saving default to false
 
   return (
     <Viz
-      vizObj={props.vizObj ? props.vizObj : localVizObj}
-      setVizObj={props.setVizObj ? props.setVizObj : setLocalVizObj}
+      vizObj={props.vizObj ? props.vizObj : vizObj}
       interactive={props.interactive ? props.interactive : localInteractive}
       setInteractive={props.setInteractive ? props.setInteractive : setLocalInteractive}
       vizUrl={props.vizUrl}
