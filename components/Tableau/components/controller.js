b215d6dbda78940bc990d49a0a9d25c0b2bda53f
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-11-16 14:23:31 -0600

    created first successful tanstack hook with query lifecycles

--- components/Tableau/components/controller.js
+++ components/Tableau/components/controller.js
@@ -1,40 +1,27 @@
 import { useQuery, } from "@tanstack/react-query"
 import { useState, useEffect } from "react";
-// tanstack query keys structured as recommended by tkdodo: https://tkdodo.eu/blog/effective-react-query-keys#structure
+// implements custom hooks with tanstack query for asynchronous state management of Tableau
+// concepts described here: https://tkdodo.eu/blog/react-query-as-a-state-manager
+// more on query key structure: https://tkdodo.eu/blog/effective-react-query-keys#structure
+// more on dependent queries: https://tanstack.com/query/v3/docs/react/guides/dependent-queries
 
-// manages filter state
 export const useFilters = async (activeSheet, id) => {
-  // enables dependent queries: https://tanstack.com/query/v3/docs/react/guides/dependent-queries
   const active = !activeSheet ? false : true;
-
-  let filters = [];
 
   async function syncFilters() {
     // For more information, see https://help.tableau.com/current/api/embedding_api/en-us/docs/embedding_api_filter.html
     if (activeSheet) {
       try {
-        return filters = await activeSheet.getFiltersAsync();
-
-        // // show # and names of categorical filters in the Console
-        // categoricalFilters = dashFilters.filter((df) => df.filterType === FilterType.Categorical);
-        // nonCategoricalFilters = dashFilters.filter((df) => df.filterType !== FilterType.Categorical);
-
-        // console.log(`The number of categorical filters: ${filter.length} 
-        // Filters: ${filter.map((s) => s.fieldName)}`);
-        // console.log('Available categorical dashboard filters:', filter);
-        
+        return await activeSheet.getFiltersAsync();
       } catch (e) {
         console.error(e.toString());
       };
     }
   };
 
-  const result = useQuery({
-    queryKey: ['hook','tableau', 'viz', 'filters', id],
-    queryFn: () => syncFilters(),
+  return useQuery({
+    queryKey: ['tableau', 'viz', 'filters', id],
+    queryFn: async () => await syncFilters(),
     enabled: active,
-    initialData: []
   });
-
-  return result;
 }
--- components/Tableau/tableau.js
+++ components/Tableau/tableau.js
@@ -1,5 +1,4 @@
 import { useState, useId, useRef, forwardRef, useEffect } from 'react'
-import { useQuery } from '@tanstack/react-query'
 import { useFilters } from './components/controller'
 import View from './components/view'
 import Model from './components/model'
@@ -7,76 +6,49 @@ import Model from './components/model'
 // HOC following MVC pattern: https://medium.com/swlh/elements-of-mvc-in-react-9382de427c09, forwardRef HOC receives ref from parent
 const Tableau = forwardRef(function Tableau(props, ref) {
   const vizLocal = useRef(null); // useRef accesses DOM nodes created with the render method https://reactjs.org/docs/refs-and-the-dom.html
+  const id = `id-${useId()}`; // creates a unique identifier for the embed
   const [vizObj, setVizObj] = useState(null);
   const [localInteractive, setLocalInteractive] = useState(false);
-  const id = `id-${useId()}`; // creates a unique identifier for the embed
+  const [dashboard, setDashboard] = useState(null);
   const viz = props.viz ? props.viz.current : vizLocal.current;
   const interactive = props.interactive ? props.interactive : localInteractive;
 
-  const [dashboard, setDashboard] = useState(null);
-
-  const active = !dashboard ? false : true;
-
-  let filters = [];
-
-  async function syncFilters() {
-    // For more information, see https://help.tableau.com/current/api/embedding_api/en-us/docs/embedding_api_filter.html
-    if (!dashboard) {
-      filters = [];
-    } else {
-      try {
-        return filters = await dashboard.getFiltersAsync();
-
-        // // show # and names of categorical filters in the Console
-        // categoricalFilters = dashFilters.filter((df) => df.filterType === FilterType.Categorical);
-        // nonCategoricalFilters = dashFilters.filter((df) => df.filterType !== FilterType.Categorical);
-
-        // console.log(`The number of categorical filters: ${filter.length} 
-        // Filters: ${filter.map((s) => s.fieldName)}`);
-        // console.log('Available categorical dashboard filters:', filter);
-        
-      } catch (e) {
-        console.error(e.toString());
-      };
-    }
-  };
-
-  const result = useFilters(dashboard, id);
-
-  const result2 = useQuery({
-    queryKey: ['tableau', 'viz', 'filters', id],
-    queryFn: () => syncFilters(),
-    enabled: active,
-    initialData: []
-  });
-
-  // if (result.isLoading) {
-  //   console.log('loading data:', result.data);
-  // }
-  // if (result.isError) {
-  //   console.error('data error:', result.error);
-  // }
-  // if (result.isSuccess) {
-  //   console.log('success data:', result.data);
-  // }
-
-  // console.log('result:', result);
-
-
-
   useEffect(() => {
-    function getDashboard() {
-      if (viz) {
-        // get the active sheet which may be a dashboard, sheet or story
-        setDashboard(viz.workbook.activeSheet);
-      }
-    }
-
     if (!dashboard && interactive) {
       getDashboard();
     }
-  }, [viz, interactive, dashboard]);
+  }, [dashboard, interactive]);
 
+  const syncTab = async () => {
+    const { status, isFetched, isLoading, isSuccess, data, isError, error } =  await useFilters(dashboard, id);
+
+    if (status === 'pending') {
+      console.log('requesting data...');
+      if (isLoading) {
+        console.log('loading data...');
+      }
+    } else if (status === 'success') {
+      console.log(status, `The number of filters is: ${data.length}`, data);
+      // show # and names of categorical filters in the Console
+      // categoricalFilters = dashFilters.filter((df) => df.filterType === FilterType.Categorical);
+      // nonCategoricalFilters = dashFilters.filter((df) => df.filterType !== FilterType.Categorical);
+      // console.log(`Filters: ${filter.map((s) => s.fieldName)}`);
+      // console.log('Available categorical dashboard filters:', filter);
+    }
+
+    
+  }
+
+  syncTab();
+
+  const getDashboard = () => {
+    if (viz) {
+      // get the active sheet which may be a dashboard, sheet or story
+      setDashboard(viz.workbook.activeSheet);
+    }
+  }
+
+  
   return (
     <View
       vizObj={props.vizObj ? props.vizObj : vizObj}
