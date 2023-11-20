c887ee07e75ff0d1927f9bac4418abf7cae0f7b4
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-11-19 23:17:51 -0600

    initial insights hook, TODO: redo auth

--- components/pulse/useInsights.js
+++ components/pulse/useInsights.js
@@ -5,10 +5,57 @@ import { useState, useEffect } from "react";
 // more on query key structure: https://tkdodo.eu/blog/effective-react-query-keys#structure
 // more on dependent queries: https://tanstack.com/query/v3/docs/react/guides/dependent-queries
 
+export const useRestAuth = async () => {
+  const tableau = {
+    url: 'https://us-west-2a.online.tableau.com',
+    site: 'pulseinternal',
+    apiVersion: '3.18',
+    pat: {
+        name: 'takashi-test',
+        secret: 'D7f8iW1HSkCDqS7mxA6ENA==:7AOE2FwChb2TwMxx7WKWTulafY4uspOh'
+    }
+  }
+
+  async function tableauAuth() {
+    const url = `${tableau.url}/api/${tableau.apiVersion}/auth/signin`;
+    const options = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            "credentials": {
+                "personalAccessTokenName": tableau.pat.name,
+                "personalAccessTokenSecret": tableau.pat.secret,
+                "site": {
+                    "contentUrl": tableau.site
+                }
+            }
+        })
+    }
+  
+    const resp = await fetch(url,options);
+    if (resp.ok) {
+        const data = await resp.json();
+        return {
+            'apiToken': data.credentials.token,
+            'userId': data.credentials.user.id
+        };
+    } else {
+        return null;
+    }
+  }
+
+  return useQuery({
+    queryKey: ['tableau', 'auth'],
+    queryFn: async () => await tableauAuth(),
+  });
+}
+
 export const useInsights = async () => {
 
   const getInsights = async () => {
-
   }
 
   return useQuery({
