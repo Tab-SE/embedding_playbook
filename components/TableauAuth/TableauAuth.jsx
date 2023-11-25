ae19b1d1ceeeaaa95f31bebcd9f889b8032f7dcf
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-11-24 19:42:14 -0600

    showing data from the user object

--- components/TableauAuth/TableauAuth.jsx
+++ components/TableauAuth/TableauAuth.jsx
@@ -2,21 +2,21 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function TableauAuth() {
   // documented here: https://next-auth.js.org/getting-started/client#usesession
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   const res = useSession();
 
-  if (session) {
-    console.log('session', session);
+  if (res.status === 'authenticated') {
+    console.log('res', res);
     return (
       <>
-        Status is {res.status} <br />
+        {res.data.user.name} is {res.status} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     )
   }
   return (
     <>
-      Status is {res.status} <br />
+      User is {res.status} <br />
       <button onClick={() => signIn()}>Sign in</button>
     </>
   )
