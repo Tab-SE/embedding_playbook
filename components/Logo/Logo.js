14d3284d68d5270e89defb1271b5e7e735b001fb
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-11-26 23:36:47 -0600

    fixed SVG logo

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,17 +1,16 @@
-import Img from '../Img/Img'
-import Image from 'next/image'
+import LogoColor from '../../public/svg/logo_color.svg'
 
 function Logo(props) {
 
   return (
   <>
   <span className='mr-2'>
-    <Image
+    <LogoColor 
       src={props.src ? props.src : '/svg/logo_color.svg?w=40&h=40'}
       alt={props.alt ? props.alt : 'Tableau Software (logo)'}
       width={props.width ? props.width : 40}
       height={props.width ? props.width : 40}
-      full={props.full ? props.full : 'false'}
+      full={props.full ? props.full : 'false'} 
     />
   </span>
   <p className='invisible md:visible'>
