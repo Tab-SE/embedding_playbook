029f787579bf1b3947ff9d75af4d1e2c913889d9
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-11 16:37:08 -0600

    fixed envvar assignments

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,12 @@
 // exports a config object containing environment variables and other app settings
 // avoids running process.env function calls on server functions due to minor performance impact
+
 const config = {
   node_env: process.env.NODE_ENV,
-  public_api: NEXT_PUBLIC_API_BASE_URL,
+  public_api: process.env.NEXT_PUBLIC_API_BASE_URL,
   auth: {
-    auth_url: NEXTAUTH_URL,
-    auth_secret: NEXTAUTH_SECRET,
+    auth_url: process.env.NEXTAUTH_URL,
+    auth_secret: process.env.NEXTAUTH_SECRET,
     github_id: process.env.GITHUB_ID, 
     github_secret: process.env.GITHUB_SECRET,
   }, 
