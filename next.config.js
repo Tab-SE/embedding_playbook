5bdc962d9d4e6e5f2623ee2622a6335ac4e37aa1
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-11 00:48:06 -0600

    commented out github actions, may remove

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
-const path = process.env.PROJECT_NAME;
 // used by <Link/> component and static CSS
 let assetPrefix = '';
 // used by <Image/> component
 let basePath = '';
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-// exports via GitHub Pages via automated Actions
-// https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
-if (isGithubActions) {
-  // trim off `<owner>/`, testing dynamically obtaining values from GitHub env
-  // const path = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${path}/`;
-  basePath = `/${path}`;
-} else if (process.env.HOSTING === 'gh_pages') {
-  // exporting to GitHub Pages via package.json scripts
-  assetPrefix = `/${path}/`;
-  basePath = `/${path}`;
-}
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// // exports via GitHub Pages via automated Actions
+// // https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
+// if (isGithubActions) {
+//   const path = process.env.PROJECT_NAME;
+//   // trim off `<owner>/`, testing dynamically obtaining values from GitHub env
+//   // const path = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+//   assetPrefix = `/${path}/`;
+//   basePath = `/${path}`;
+// } else if (process.env.HOSTING === 'gh_pages') {
+//   // exporting to GitHub Pages via package.json scripts
+//   assetPrefix = `/${path}/`;
+//   basePath = `/${path}`;
+// }
 
 // initializes the withNextra config object with theme settings
 // set staticImage: false to avoid usage of IMGIX at a loss of image optimization
@@ -29,12 +29,12 @@ const withNextra = require('nextra')({
 module.exports = withNextra({
   assetPrefix: assetPrefix,
   webpack(config) {
+    // uses svgr for safe usage of SVGs in React
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     })
-    // uses svgr for safe usage of SVGs in React
     return config
   },
 });
