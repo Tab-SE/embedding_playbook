6af2f73af6f8e6e9d34004267500b3988929cf7f
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-11 19:14:14 -0600

    simplified config object so it focuses on tableau auth

--- utils/config.js
+++ utils/config.js
@@ -2,30 +2,20 @@
 // avoids running process.env function calls on server functions due to minor performance impact
 
 const config = {
-  node_env: process.env.NODE_ENV,
-  public_api: process.env.NEXT_PUBLIC_API_BASE_URL,
-  auth: {
-    auth_url: process.env.NEXTAUTH_URL,
-    auth_secret: process.env.NEXTAUTH_SECRET,
-    github_id: process.env.GITHUB_ID, 
-    github_secret: process.env.GITHUB_SECRET,
-  }, 
-  tableau : {
-    viz: {
-      domain: process.env.TABLEAU_DOMAIN,
-      api: process.env.TABLEAU_API,
-      site: process.env.TABLEAU_SITE,
-      pat_name: process.env.TABLEAU_PAT_NAME,
-      pat_secret: process.env.TABLEAU_PAT_SECRET,
-    },
-    pulse: {
-      domain: process.env.PULSE_DOMAIN,
-      api: process.env.PULSE_API,
-      site: process.env.PULSE_SITE,
-      pat_name: process.env.PULSE_PAT_NAME,
-      pat_secret: process.env.PULSE_PAT_SECRET,
-    } 
-  }
+  viz: {
+    domain: process.env.TABLEAU_DOMAIN,
+    api: process.env.TABLEAU_API,
+    site: process.env.TABLEAU_SITE,
+    pat_name: process.env.TABLEAU_PAT_NAME,
+    pat_secret: process.env.TABLEAU_PAT_SECRET,
+  },
+  pulse: {
+    domain: process.env.PULSE_DOMAIN,
+    api: process.env.PULSE_API,
+    site: process.env.PULSE_SITE,
+    pat_name: process.env.PULSE_PAT_NAME,
+    pat_secret: process.env.PULSE_PAT_SECRET,
+  } 
 };
 
 export default config;
