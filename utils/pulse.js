f9a18f6c13e4185ed0e50adbfe7cb3706940ac79
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-11 22:22:49 -0600

    fixed domain

--- utils/pulse.js
+++ utils/pulse.js
@@ -7,7 +7,7 @@ const { domain, api, site, pat_name, pat_secret } = config.pulse;
 export const getSubscriptions = async (apiKey, userId, pageSize) => {
   try {
     // console.log(`AUTH ATTEMPT: ${key}`, value);
-    const res = await axios.get(`${public_url}/api/-/subscriptionservice/subscriptions`, {
+    const res = await axios.get(`${domain}/api/-/subscriptionservice/subscriptions`, {
       params: {
         user_id: userId,
         page_size: pageSize ? pageSize : 50,
@@ -34,33 +34,5 @@ export const getSubscriptions = async (apiKey, userId, pageSize) => {
 
 export const getMetricDefinitions = async (apiKey) => {
   // TODO: scopes and core metrics
-}
-
-export const getInsights = async (apiKey) => {
-  try {
-    // console.log(`AUTH ATTEMPT: ${key}`, value);
-    const res = await axios.get(`${domain}/api/-/subscriptionservice/subscriptions`, {
-      params: {
-        user_id: userId,
-        page_size: pageSize ? pageSize : 50,
-      },
-      headers: {
-        "X-Tableau-Auth": apiKey,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      }
-    });
-
-    if (res.status === 200){
-      console.log('getSubscriptions:', res);
-      const data = res.data;
-      return data.subscriptions;
-    } else {
-      throw new Error('ERROR: Cannot obtain subscriptions!');
-    }
-  } catch (err) {
-    console.debug(err);
-    return null;
-  }
 }
 
