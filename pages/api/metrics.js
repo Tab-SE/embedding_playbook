e4d97e4ecf85a2d956cfdde03b12d0de22d66286
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-18 10:07:07 -0600

    returning an object including processed values

--- pages/api/metrics.js
+++ pages/api/metrics.js
@@ -20,17 +20,17 @@ const handler = async (req, res) => {
       try {
         const metrics = new Metrics(user); // instantiate a new Metrics object for the logged in user
         const subscriptions = await metrics.getSubscriptions(key);
-        res.status(200).json({ subscriptions });
-        // if (Object.keys(metrics.subscriptions).length > 0) { // validate that the subscriptions node is not empty
-        //   console.log('metrics.subscriptions', metrics.subscriptions);
-        //   const subscriptions = metrics.subscriptions;
-        //   res.status(200).json({ subscriptions });
-        // } else {
-        //   throw new Error('Failed to obtain Tableau Pulse Metrics');
-        // }
+        const scoped_metrics = await metrics.getScopedMetrics(key);
+        const payload = { subscriptions: subscriptions, scoped_metrics: scoped_metrics };
+        console.log(payload);
+        res.status(200).json(payload);
       } catch (err) {
         res.status(500).json({ error: err });
       }
+
+
+
+
     } else {
       res.status(405).json({ error: 'Method Not Allowed' });
     }
@@ -41,6 +41,7 @@ const handler = async (req, res) => {
   }
   res.end();
 }
+
 
 export default handler;
 
