7729a150f00dde7bb768db1623195d3d9c764c7d
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-17 22:48:43 -0600

    auth component is initializing tanstack query with an empty query key until requests are sent

--- components/TableauAuth/TableauAuth.jsx
+++ components/TableauAuth/TableauAuth.jsx
@@ -1,25 +1,33 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { useMetrics } from '../../models/Metrics';
 
 export default function TableauAuth() {
-  // documented here: https://next-auth.js.org/getting-started/client#usesession
-  // const { data: session } = useSession();
+  const [userID, setUserID] = useState(undefined);
   const res = useSession();
+  const metrics = useMetrics(userID);
+
+  useEffect(() => {
+    if (res.status === 'authenticated') {
+      console.log('res', res);
+      setUserID('a3302788-5406-4ab7-bbe3-e2dd39b9eb6f');
+    }
+  }, [res.status]);
 
   if (res.status === 'authenticated') {
-    console.log('res', res);
-
     return (
       <>
         {res.data.user.name} is {res.status} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
-    )
+    );
   }
+
   return (
     <>
       User is {res.status} <br />
       <button onClick={() => signIn()}>Sign in</button>
     </>
-  )
+  );
 }
