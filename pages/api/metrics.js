a1d7735387c4f699d16a03d30ab5d1a18ce8c6ae
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-20 01:18:25 -0600

    apiKey needed for all HTTP calls

--- pages/api/metrics.js
+++ pages/api/metrics.js
@@ -14,27 +14,23 @@ const handler = async (req, res) => {
     // console.log("Server Session", JSON.stringify(session, null, 2));
 
     // get user attributes for temporary authorized sessions
-    const { key, user } = token.rest.pulse;
+    const { user, key } = token.rest.pulse;
 
     if (req.method === 'GET') {
       try {
         const metrics = new Metrics(user); // instantiate a new Metrics object for the logged in user
         const payload = await metrics.getMetrics(key); // request metrics for the user
-        console.log('P A Y L O A D ! :', JSON.stringify(payload, null, 2));
+        // console.log('P A Y L O A D ! :', JSON.stringify(payload, null, 2));
         res.status(200).json(payload);
       } catch (err) {
         res.status(500).json({ error: err });
       }
-
-
-
-
     } else {
       res.status(405).json({ error: 'Method Not Allowed' });
     }
   } else {
     // Not Signed in
-    console.log('unauthorized');
+    console.debug('unauthorized');
     res.status(401).json({ error: 'Unauthorized' });
   }
   res.end();
