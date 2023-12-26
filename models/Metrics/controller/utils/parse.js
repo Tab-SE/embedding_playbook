27026831d5225eaf7dd5bd89106abf211b61f405
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-26 15:17:09 -0500

    logging input objects for testing

--- models/Metrics/controller/utils/parse.js
+++ models/Metrics/controller/utils/parse.js
@@ -74,27 +74,59 @@ export const parseSpecifications = (specificationsResponse) => {
 
 
  // return a minimal representation of metrics
-export const parseMetrics = (metricsObj) => {
-  // console.log('parse metricsObj', metricsObj);
-  // const subscriptions = JSON.stringify(subscriptions);
-  // const specifications = JSON.stringify(specifications);
-  // const definitions = JSON.stringify(definitions);
-
-
-  const metrics = {};
+export const parseMetrics = (subscriptionsObj, specificationsObj, definitionsObj) => {
+  // console.log('subscriptionsObj', JSON.stringify(subscriptionsObj, null, 2));
+  // console.log('specificationsObj', JSON.stringify(specificationsObj, null, 2));
+  // console.log('definitionsObj', JSON.stringify(definitionsObj, null, 2));
+  
+  const metrics2 = {};
+  const metrics = [];
 
   // Retrieve properties using JSONPath
-  const names = JSONPath({ path: '$.subscriptions[*].id', json: subscriptions }); // indexing array
-  const id = JSONPath({ path: '$.subscriptions[*].id', json: subscriptions });
+
+  // definitions
+  const names = JSONPath({ path: '$.*.name', json: definitionsObj }); // indexing array
+  const description = JSONPath({ path: '$.*.description', json: definitionsObj });
+  const definition_id = JSONPath({ path: '$.*.id', json: definitionsObj });
+  const definition = JSONPath({ path: '$.*.definition', json: definitionsObj });
+  const extension_options = JSONPath({ path: '$.*.extension_options', json: definitionsObj });
+  const representation_options = JSONPath({ path: '$.*.representation_options', json: definitionsObj });
+  const insights_options = JSONPath({ path: '$.*.insights_options', json: definitionsObj });
+  // specifications
+  const specification_id = JSONPath({ path: '$.*.specification_id', json: specificationsObj });
+  const specification = JSONPath({ path: '$.*.specification', json: specificationsObj });
+  const definition_key = JSONPath({ path: '$.*.definition_id', json: specificationsObj });
+  // subscriptions
+  const subscription_id = JSONPath({ path: '$.*.subscription_id', json: subscriptionsObj });
+  const specification_key = JSONPath({ path: '$.*.metric_id', json: subscriptionsObj });
+
+  Object.entries(definitionsObj).forEach(([key, value]) => {
+    metrics.push(
+      
+    )
+  });
+
+
 
   // Iterate through indexing array and create leaves in the return object
   names.forEach((name, index) => {
-    metrics[index] = {
+    metrics2[index] = {
       name: name, 
-      id: id[index], // Add the corresponding properties by index
+      description: description[index], // Add the corresponding properties by index
+      subscription_id: subscription_id[index],
+      specification_id: specification_id[index],
+      specification_key: specification_key[index],
+      specification: specification[index],
+      definition_id: definition_id[index],
+      definition_key: definition_key[index], 
+      definition: definition[index],
+      extension_options: extension_options[index],
+      representation_options: representation_options[index],
+      insights_options: insights_options[index],
     };
   });
 
-  console.log('metrics', metrics);
+  // console.log('METRICS', metrics);
+
   return metrics;
 }
