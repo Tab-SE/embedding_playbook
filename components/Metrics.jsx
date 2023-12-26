692efe1ac06339c27d26ac735662d4c586fc5a40
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-26 16:06:11 -0500

    rendering Insights component rather than individual insights

--- components/Metrics.jsx
+++ components/Metrics.jsx
@@ -1,25 +1,10 @@
-import PowerLevel from '../models/Metrics/mocks/PowerLevel/PowerLevel.json'
-import { BAN, Detail } from './Insights'
+import Insights from './Insights'
 
 function Metrics(props) {
-  const insights = parseInsights(PowerLevel);
 
   return (
-    <>
-    <BAN insights={insights} />
-    <Detail insights={insights} />
-    </>
+    <Insights title={props.title} />
   )
 }
-
-
-const parseInsights = (metric) => {
-  const insights = {};
-  metric.result.insight_groups.forEach(group => {
-    insights[group.type] = group;
-  });
-  return insights;
-}
-
 
 export default Metrics;
