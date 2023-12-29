c3ac4823194b1b7eb004c48e80da334f01a1037e
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-29 12:18:21 -0600

    refactored button

--- components/TableauAuth.jsx
+++ components/TableauAuth.jsx
@@ -4,28 +4,44 @@ import { useMetrics } from '../models/Metrics';
 
 export default function TableauAuth() {
   const [userID, setUserID] = useState(undefined);
+  // const { status, data } = useSession({
+  //   required: true,
+  //   onUnauthenticated() {
+  //     // The user is not authenticated, handle it here.
+  //   },
+  // })
   const sesh = useSession();
   const metrics = useMetrics(userID);
 
   useEffect(() => {
     if (sesh.status === 'authenticated') {
       setUserID('a3302788-5406-4ab7-bbe3-e2dd39b9eb6f');
+      console.log('metrics for user:', sesh.data.user);
     }
   }, [sesh]);
+
+  if (sesh.status === 'loading') {
+    return (
+      <>
+        <p className="nx-font-bold m-4">{sesh.status}</p> 
+        <button className="btn glass" onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
 
   if (sesh.status === 'authenticated') {
     return (
       <>
-        <p className="nx-font-bold mt-4">{sesh.data.user.name} is {sesh.status}</p> 
-        <button className="nx-text-xl nx-font-bold" onClick={() => signOut()}>Sign out</button>
+        <p className="nx-font-bold m-4">{sesh.data.user.name} is {sesh.status}</p> 
+        <button className="btn glass" onClick={() => signOut()}>Sign out</button>
       </>
     );
   }
 
   return (
     <>
-      <p className="nx-font-bold mt-4">User is {sesh.status}</p> 
-      <button className="nx-text-xl nx-font-bold" onClick={() => signIn()}>Sign in</button>
+      <p className="nx-font-bold m-4">User is {sesh.status}</p> 
+      <button className="btn glass" onClick={() => signIn()}>Sign in</button>
     </>
   );
 }
