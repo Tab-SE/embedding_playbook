864ef881b44cc5cffe4b9fcab78f4c3440761ce8
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-30 09:01:26 -0600

    removed unused config branch

--- utils/config.js
+++ utils/config.js
@@ -2,13 +2,6 @@
 // avoids running process.env function calls on server functions due to minor performance impact
 
 const config = {
-  viz: {
-    domain: process.env.TABLEAU_DOMAIN,
-    api: process.env.TABLEAU_API,
-    site: process.env.TABLEAU_SITE,
-    pat_name: process.env.TABLEAU_PAT_NAME,
-    pat_secret: process.env.TABLEAU_PAT_SECRET,
-  },
   pulse: {
     domain: process.env.PULSE_DOMAIN,
     api: process.env.PULSE_API,
