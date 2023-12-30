aaa3ce78fe12293a8cc9d23bc33c6c6b4267e797
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-30 14:01:31 -0600

    added tabhSigin

--- libs/index.js
+++ libs/index.js
@@ -1,4 +1,5 @@
 export const tab_embed = typeof window !== 'undefined' ? require("./tableau.embedding.3.latest.min.js") : null;
+
 export { 
-  getSubscriptions, getSpecifications, getDefinitions, getMetrics, getInsights, getInsightBundles 
-} from './requests'
+  tabSignIn, getSubscriptions, getSpecifications, getDefinitions, getMetrics, getInsights, getInsightBundles 
+} from './requests';
