0804b978479e212a18c27309e146c346e13eedbe
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-31 19:52:41 -0600

    using new parser and mock

--- components/Insights/Detail.jsx
+++ components/Insights/Detail.jsx
@@ -1,4 +1,6 @@
-import VegaLiteViz from "../VegaLiteViz";
+import { useEffect } from "react";
+import { parseBan } from "../../utils/parse";
+import bundle from "../../models/Insights/mocks/generate_detail_insight.json"
 
 function Detail(props) {
   const summaries = props.insights?.top?.summaries[0]?.result;
@@ -7,6 +9,17 @@ function Detail(props) {
   const markup = insights?.markup;
   const value = insights?.facts?.target_period_value?.formatted;
   const label = insights?.facts?.target_period_value?.label;
+
+  // returns a minimal representation for the UI
+  const parsedBundle = parseBan(bundle);
+  // const { value, question, markup, viz  } = parsedBundle[0];
+
+  // console.log('parsedDetail', parsedBundle);
+
+  useEffect(() => {
+    // the metric value is controlled in the parent component but set here
+    props.pushQA(value);
+  }, [value]);
 
   return (
     <p dangerouslySetInnerHTML={{__html: markup}} /> 
