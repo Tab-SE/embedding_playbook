1a88f0faa790e84a90c324dd6a907a13cba43a2d
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-31 19:53:02 -0600

    added Springboard

--- components/Insights/Springboard.jsx
+++ components/Insights/Springboard.jsx
@@ -1,21 +1,29 @@
+import { useEffect } from "react";
 import VegaLiteViz from "../VegaLiteViz";
+import { parseBan } from "../../utils/parse";
+import bundle from "../../models/Insights/mocks/generate_springboard_insight.json"
 
 function Springboard(props) {
-  const summaries = props.insights.ban.summaries[0].result;
-  const insights = props.insights.ban.insights[0].result;
-  const viz = summaries?.viz;
-  const markup = insights?.markup;
-  const value = insights?.facts?.target_period_value?.formatted;
-  const label = insights?.facts?.target_period_value?.label;
+   // returns a minimal representation for the UI
+   const parsedBundle = parseBan(bundle);
+   // const { value, question, markup, viz  } = parsedBundle[0];
+  //  console.log('parsedSpringboard', parsedBundle);
+
+  //  console.log('parsedBundle[0]', parsedBundle[0]?.viz);
+  //  console.log('parsedBundle[1]', parsedBundle[1]?.viz)
+
+
+   const viz = parsedBundle[1].viz;
+   const markup = parsedBundle[1].markup;
 
   return (
     <>
-    <div className='flex items-end p-4'>
+    {/* <div className='flex items-end p-4'>
       <h1 className='flex-0 nx-mt-2 nx-text-2xl nx-tracking-tight'>
         {props.title}:
       </h1>
       <p className='flex-1 nx-text-2xl nx-font-bold nx-text-slate-900 dark:nx-text-slate-100 nx-tracking-tight mx-2'>{value}</p>
-    </div>
+    </div> */}
     <VegaLiteViz height={104} spec={viz}></VegaLiteViz>
     <p dangerouslySetInnerHTML={{__html: markup}} />
     </>
