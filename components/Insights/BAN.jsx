8b6b17c0d2690c168be822b934d243e154354086
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-12-31 19:52:16 -0600

    using new parser and mocks

--- components/Insights/BAN.jsx
+++ components/Insights/BAN.jsx
@@ -1,22 +1,31 @@
+import { useEffect } from "react";
 import VegaLiteViz from "../VegaLiteViz";
+import { parseBan } from "../../utils/parse";
+import bundle from "../../models/Insights/mocks/generate_current_metric_value_insight_bundle.json"
+
 
 function BAN(props) {
-  const summaries = props.insights.ban.summaries[0].result;
-  const insights = props.insights.ban.insights[0].result;
-  const viz = summaries?.viz;
-  const markup = insights?.markup;
-  const value = insights?.facts?.target_period_value?.formatted;
-  const label = insights?.facts?.target_period_value?.label;
+  // returns a minimal representation for the UI
+  const parsedBundle = parseBan(bundle);
+  const { value, question, markup, viz  } = parsedBundle[0];
+
+  // console.log('parsedBAN', parsedBundle);
+
+  useEffect(() => {
+    // the metric value is controlled in the parent component but set here
+    props.setMetricValue(value);
+  }, [value]);
 
   return (
     <>
     <div className='flex items-end p-4'>
-      <h1 className='flex-0 nx-mt-2 nx-text-2xl nx-tracking-tight'>
+      <h1 className='flex-0 font-bold text-3xl text-stone-800'>
         {props.title}:
       </h1>
-      <p className='flex-1 nx-text-2xl nx-font-bold nx-text-slate-900 dark:nx-text-slate-100 nx-tracking-tight mx-2'>{value}</p>
+      <p className='flex-1 font-bold text-3xl text-stone-600 mx-2'>{value}</p>
     </div>
-    <VegaLiteViz height={104} spec={viz}></VegaLiteViz>
+    {Object.entries(viz).length === 0 ? <></> : <VegaLiteViz height={104} spec={viz}></VegaLiteViz>}
+    <p dangerouslySetInnerHTML={{__html: question}} />
     <p dangerouslySetInnerHTML={{__html: markup}} />
     </>
   )
