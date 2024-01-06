cdedc490b4487bf11219cf42393cbd9e13ecb004
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-01-06 16:37:45 -0600

    removed logs

--- models/Session/controller.js
+++ models/Session/controller.js
@@ -2,9 +2,6 @@ import { tabAuthPAT } from "../../libs";
 
 // Tableau REST API authentication via Personal Access Token
 export const handlePAT = async (pat_name, pat_secret) => {
-  console('authorizePAT', pat_name, pat_secret);
   const credentials = await tabAuthPAT(pat_name, pat_secret);
-
-  console('authorizePAT', credentials);
   return credentials;
 }
