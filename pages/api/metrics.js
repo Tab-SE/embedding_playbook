03e68d61586542af4d36233d97dfb857014ba805
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-01-06 16:38:08 -0600

    using pat

--- pages/api/metrics.js
+++ pages/api/metrics.js
@@ -1,5 +1,5 @@
 import { getToken } from "next-auth/jwt"
-import { MetricsModel, Session } from '../../models'
+import { Session, MetricsModel } from "../../models";
 
 
 const handler = async (req, res) => {
@@ -11,27 +11,47 @@ const handler = async (req, res) => {
   // Signed in
   if (token?.tableau) { // tableau object containing asset data for the user
     if (req.method === 'GET') {
+      let sesh;
       try {
-        const sesh = new Session(token.tableau.username); // user provided during authentication is used to create a new Session
-        const { username, user_id, rest_key, site_id, site, created, expires,
-        } = await sesh.authorizePAT(pat_name, pat_secret); // authorize to Tableau via PAT
+        sesh = new Session(token.tableau.username); // user provided during authentication is used to create a new Session
+        // const { username, user_id, rest_key, site_id, site, created, expires,
+        // } = await sesh.pat(pat_name, pat_secret); // authorize to Tableau via PAT
         // console.log(username, user_id, rest_key);
-        // request metrics
-        const metrics = new MetricsModel(user_id); // instantiate a new Metrics object for the logged in user
-        const payload = await metrics.syncMetrics(rest_key); // request metrics for the user
-        res.status(200).json(payload);
+        await sesh.pat(pat_name, pat_secret); // authorize to Tableau via PAT
       } catch (err) {
         res.status(500).json({ error: err });
       }
+      if (sesh.authorized) {
+        // spread members of the Session "sesh"
+        const { 
+          username, user_id, embed_key, rest_key, site_id, site, created, expires,
+        } = sesh;
+        const payload = await getMetrics(user_id, rest_key);
+        res.status(200).json(payload);
+      } else {
+        res.status(401).json({ error: 'Unauthorized to perform operation' }); // cannot establish a session locally
+      }
+
     } else {
       res.status(405).json({ error: 'Method Not Allowed' });
     }
   } else {
     // Not Signed in
     console.debug('unauthorized');
-    res.status(401).json({ error: 'Unauthorized' });
+    res.status(401).json({ error: 'Unauthorized' }); // no application token available
   }
   res.end();
 }
 
 export default handler;
+
+const getMetrics = async (user_id, rest_key) => {
+  try {
+    // request metrics
+    const factory = new MetricsModel(user_id); // instantiate a new Metrics object for the logged in user
+    const metrics = await factory.syncMetrics(rest_key); // request metrics for the user
+    return metrics;
+  } catch (err) {
+    throw new Error('Metrics Error:', err);
+  }
+}
