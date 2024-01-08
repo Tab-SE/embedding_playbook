b771ba052555ba7bdce5dd2303983037c852f0c9
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-01-07 22:11:29 -0600

    exporting jwt functions

--- libs/index.js
+++ libs/index.js
@@ -1,5 +1,7 @@
 export const tab_embed = typeof window !== 'undefined' ? require("./tableau.embedding.3.latest.min.js") : null;
 
 export { 
-  tabAuthPAT, getSubscriptions, getSpecifications, getDefinitions, getMetrics, getInsights, getInsightBundles 
+  tabAuthJWT, tabAuthPAT, getSubscriptions, getSpecifications, getDefinitions, getMetrics, getInsights, getInsightBundles 
 } from './requests';
+
+export { jwtEncode, jwtDecode } from './crypto';
