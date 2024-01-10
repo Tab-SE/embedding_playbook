3da0ce272828dc60e085771beeeeb1faead44fa6
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-01-10 17:34:11 -0600

    exporting new hooks

--- hooks/index.js
+++ hooks/index.js
@@ -2,5 +2,5 @@ export { useTableauSession } from './useTableauSession';
 export { useMetrics } from './useMetrics';
 export { useBan } from './useBan';
 export { useSpringboard } from './useSpringboard';
-export { useInsights } from './useInsights';
+export { useDetail } from './useDetail';
 export { useTimeout } from './useTimeout';
