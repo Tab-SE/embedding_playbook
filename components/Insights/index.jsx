69dae17e1607c99dae64ab49b13d27390eac25c7
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-01-14 01:04:09 -0600

    only using <Detail/>

--- components/Insights/index.jsx
+++ components/Insights/index.jsx
@@ -1,36 +1,22 @@
-import BAN  from './BAN'
-import Detail from './Detail'
-import Springboard from './Springboard'
-import PowerLevel from '../../models/Metrics/mocks/PowerLevel/PowerLevel.json'
+import Detail from './Detail';
 
 export default function Insights(props) {
   const { metric, stats } = props;
 
-  const insights = parseInsights(PowerLevel);
-
-
   return (
     <>
       <div className='flex items-end p-4'>
-        <h1 className='flex-0 font-bold text-4xl text-stone-800'>
+        <h1 className='flex-0 font-bold text-4xl text-stone-600'>
           {metric.name}:
         </h1>
-        <p className='flex-1 font-bold text-4xl text-stone-600 mx-2'>{stats.value}</p>
+        <p className='flex-1 font-bold text-4xl text-stone-800 mx-2'>{stats.value}</p>
       </div>
       <div className='my-8' >
         <Detail 
-          insights={insights} 
-          title={metric.name}
+          metric={metric}
+          stats={stats}
         />
       </div>
     </>
   )
-}
-
-const parseInsights = (metric) => {
-  const insights = {};
-  metric.result.insight_groups.forEach(group => {
-    insights[group.type] = group;
-  });
-  return insights;
 }
