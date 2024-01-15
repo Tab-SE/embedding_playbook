ae550d109954343a3205ca4e4265de583dcb2ba5
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-01-14 20:49:12 -0600

    stat component

--- components/Metric.jsx
+++ components/Metric.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { useBan } from "../hooks";
+import { useState, useEffect } from "react";
+import { useBan, useDetail } from "../hooks";
+import { parseDetail } from "../utils";
 import Modal from "./Modal";
 import Insights from "./Insights";
 
@@ -7,9 +8,8 @@ import Insights from "./Insights";
 export default function Metric(props) {
   const { metric } = props;
   const [modal, setModal] = useState(undefined);
-  const [bundleCount, setBundleCount] = useState(0);
   let result; // contains question, markup and facts
-  let facts; // contains values, changes
+  let facts; // contains values, absolute and relative changes
   let stats = { sentiment: undefined }; // prop storing key facts
   // tanstack query hook
   const { status, data, error, isError, isSuccess } = useBan(metric);
@@ -41,24 +41,47 @@ export default function Metric(props) {
 
   return (
     <div className="cursor-pointer" onClick={()=> modal ? modal.showModal() : false }>
-      <div className="stat h-36 w-40">
-        <div className="stat-title text-sm font-bold flex items-end align-bottom whitespace-normal h-10">{metric.name}</div>
-        <div className="stat-value text-3xl whitespace-normal">{stats.value ? stats.value : '0'}</div>
-        <div className="stat-desc whitespace-normal">
-          &nbsp; {stats.direction} {stats.absolute} {stats.relative ? `(${stats.relative})` : null}
-        </div> 
-        <div className="stat-desc whitespace-normal mt-2">
-          Insights: <span className="badge badge-sm badge-primary ml-1">{bundleCount}</span>
-        </div>      
-      </div>
+      <Stat metric={metric} stats={stats} />
       <Modal setModal={setModal} >
-        <Insights metric={metric} stats={stats} setBundleCount={setBundleCount} />
+        <Insights metric={metric} stats={stats} />
         <div className="flex justify-center gap-12 w-full">
           <kbd className="kbd kbd-lg">◀︎</kbd>
           <kbd className="kbd kbd-lg">Swipe</kbd>
           <kbd className="kbd kbd-lg">▶︎</kbd>
         </div>
       </Modal>
+    </div>
+  )
+}
+
+function Stat(props) {
+  const { metric, stats } = props;
+  const [bundleCount, setBundleCount] = useState(0);
+  // tanstack query hook
+  const { status, data, error, isError, isSuccess } = useDetail(metric);
+
+  if (isError) {
+    console.debug(error);
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      // main data found in insight groups
+      const details = parseDetail(data);
+      setBundleCount(details.length);
+    }
+  }, [isSuccess, data]);
+
+  return (
+    <div className="stat h-36 w-40">
+      <div className="stat-title text-sm font-bold flex items-end align-bottom whitespace-normal h-10">{metric.name}</div>
+      <div className="stat-value text-3xl whitespace-normal">{stats.value ? stats.value : '0'}</div>
+      <div className="stat-desc whitespace-normal">
+        &nbsp; {stats.direction} {stats.absolute} {stats.relative ? `(${stats.relative})` : null}
+      </div> 
+      <div className="stat-desc whitespace-normal mt-2">
+        Insights: <span className="badge badge-sm badge-primary ml-1">{bundleCount}</span>
+      </div>      
     </div>
   )
 }
