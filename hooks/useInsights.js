c66418232b51d94c3da0067a2709b63ee6412571
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-01-23 13:09:05 -0600

    replaced with getInsights()

--- hooks/useInsights.js
+++ hooks/useInsights.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
-import { getDetail } from "../libs/requests"
+import { getDetail, getInsights } from "../libs/requests"
 // implements custom hooks with tanstack query for asynchronous state management
 // concepts described here: https://tkdodo.eu/blog/react-query-as-a-state-manager
 // more on query key structure: https://tkdodo.eu/blog/effective-react-query-keys#structure
@@ -7,12 +7,12 @@ import { getDetail } from "../libs/requests"
 
 export const useInsights = (metric) => {
   // set to an empty array if enumerated function parameters are not available in array
-  const queryKey = [metric].every(param => param != null) ? ["tableau", "insights", "detail", metric.id, metric.name] : []; 
+  const queryKey = [metric].every(param => param != null) ? ["tableau", "insights", metric.id, metric.name] : []; 
 
   return useQuery({
     queryKey: queryKey, 
     queryFn: () => {
-      return getDetail(metric);
+      return getInsights(metric);
     },
     enabled: !!metric,
     staleTime: 5 * 60 * 1000, // 5 minutes
