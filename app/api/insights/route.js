e3598a1487e760ec140dce15f10a594c947827f6
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-01-25 00:26:16 -0600

    longest timeout allowed

--- app/api/insights/route.js
+++ app/api/insights/route.js
@@ -1,4 +1,4 @@
-export const maxDuration = 120; // This function can run for a maximum of 120 seconds
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 import { getToken } from 'next-auth/jwt';
