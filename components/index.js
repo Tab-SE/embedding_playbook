82532060954a905bfe24c573331b657642e41839
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-02 11:07:00 -0600

    exporting components

---
+++ components/index.js
@@ -0,0 +1,11 @@
+export { Analytics } from './Analytics';
+export { Carousel } from './Carousel';
+export { Head } from './Head';
+export { Hero } from './Hero';
+export { Insights } from './Insights';
+export { Logo } from './Logo';
+export { Metric } from './Metric';
+export { Modal } from './Modal';
+export { TableauAuth } from './TableauAuth';
+export { TableauViz } from './TableauViz';
+export { VegaLiteViz } from './VegaLiteViz';
