7151f2ad6380f9db4896896988921b03a30a655b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-02 17:45:02 -0600

    fixing ref

--- components/Analytics.jsx
+++ components/Analytics.jsx
@@ -5,27 +5,27 @@ import Toolbar from './Toolbar';
 import { useTableauSession } from '../hooks';
 
 export const Analytics = (props) => {
-  const viz = useRef(null); // useRef accesses DOM nodes created with the render method https://reactjs.org/docs/refs-and-the-dom.html
+  const { src, height, width, hideTabs, device } = props;
+  // useRef accesses DOM nodes created with the render method https://reactjs.org/docs/refs-and-the-dom.html
+  const ref = useRef(null); 
   const [interactive, setInteractive] = useState(false);
   
   const authenticated = useTableauSession();
 
   return (
     <section className='bg-colorblind-lgray rounded'> 
-      <Toolbar viz={viz.current} interactive={interactive} />
+      <Toolbar viz={ref.current} interactive={interactive} />
       <div className='min-h-[63rem] my-2'>
         <div className='block h-40 items-center justify-center'>
           <Metrics /> 
         </div>
         <TableauViz
-          interactive={interactive}
-          setInteractive={setInteractive}
-          viz={viz}
-          src={props.src}
-          height={props.height}
-          width={props.width}
-          hideTabs={props.hideTabs}
-          device={props.device}
+          ref={ref}
+          src={src}
+          height={height}
+          width={width}
+          hideTabs={hideTabs}
+          device={device}
         />
       </div>
     </section>
