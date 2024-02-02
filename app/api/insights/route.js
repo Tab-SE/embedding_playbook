2612816e0ee89aaeb67650810db0e716f5d4098c
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-01 20:49:34 -0600

    refactored to use NextResponse

--- app/api/insights/route.js
+++ app/api/insights/route.js
@@ -1,13 +1,15 @@
+import { NextResponse } from "next/server";
 import { getToken } from 'next-auth/jwt';
 import { makePayload } from './methods';
 
 export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
+// responds with generated Pulse Insights for the provided metric
 export async function POST(req) {
   // Check if req is defined
   if (!req) {
-    return new Response('Bad Request', { status: 400 });
+    return NextResponse.json({ error: '400: Bad Request' }, { status: 400 });
   }
 
   // session token specific to each user
@@ -18,8 +20,13 @@ export async function POST(req) {
     const requestBody = await req.json();
     // only responds with data to authorized users
     const payload = await makePayload(token.tableau.rest_key, requestBody.metric);
-    return new Response(payload, { status: 201 });
+    if (payload) {
+      return NextResponse.json(payload, { status: 200 });
+    } else {
+      return NextResponse.json({ error: '500: Internal error: cannot generate payload' }, { status: 500 });
+    }
   } else {
-    return new Response('401: Unauthorized', { status: 401 });
+    // Not Signed in
+    return NextResponse.json({ error: '401: Unauthorized' }, { status: 401 });
   }  
 };
