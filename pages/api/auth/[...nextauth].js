1bed40a2f4c3bc2391a4644848f8033aa172bd36
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-06 15:07:03 -0600

    dual session strategy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -32,7 +32,6 @@ export const authOptions = {
         // You can also use the `req` object to obtain additional parameters
         // (i.e., the request IP address)
         let user = null;
-        let sesh = null;
         for (const [key, value] of Object.entries(rls.users)) { // check all keys in rls.json user store
           if (key.toUpperCase() === credentials.ID.toUpperCase()) { // find keys that match credential
             user = value; // if a match is found store value as user
@@ -42,47 +41,58 @@ export const authOptions = {
           // server-side env vars
           const pat_name = process.env.PULSE_PAT_NAME;
           const pat_secret = process.env.PULSE_PAT_SECRET;
-          const jwt_secret = process.env.TABLEAU_JWT_SECRET; 
-          const jwt_secret_id = process.env.TABLEAU_JWT_SECRET_ID; 
           const jwt_client_id = process.env.TABLEAU_JWT_CLIENT_ID;
+          const embed_secret = process.env.TABLEAU_EMBED_JWT_SECRET; 
+          const embed_secret_id = process.env.TABLEAU_EMBED_JWT_SECRET_ID; 
+          const rest_secret = process.env.TABLEAU_REST_JWT_SECRET; 
+          const rest_secret_id = process.env.TABLEAU_REST_JWT_SECRET_ID; 
 
-          // const rest_scopes = [
-          //   'tableau:views:embed',
-          //   'tableau:views:embed_authoring',
-          //   'tableau:insights:embed',
-          // ];
-
-          // const embed_scopes = [
-          //   'tableau:views:embed',
-          //   'tableau:views:embed_authoring',
-          //   'tableau:insights:embed',
-          // ];
-
-          const scopes = [
-            'tableau:views:embed',
-            'tableau:views:embed_authoring',
-            'tableau:insights:embed',
+          // used for frontend embeds
+          const embed_scopes = [
+            "tableau:views:embed",
+            "tableau:views:embed_authoring",
+            "tableau:insights:embed",
           ];
+          const embed_options = { 
+            jwt_secret: embed_secret, 
+            jwt_secret_id: embed_secret_id, 
+            jwt_client_id 
+          };
+          const embed_session = new Session(user.name); 
+          await embed_session.jwt(user.email, embed_options, embed_scopes);
 
-          // user provided during authentication is used to create a new Session
-          sesh = new Session(user.name); 
-          const jwt_options = { jwt_secret, jwt_secret_id, jwt_client_id };
-          // authorize to Tableau via JWT
-          await sesh.jwt(user.email, jwt_options, scopes);
-          const rest_sesh = new Session(user.name);
-          await rest_sesh.pat(pat_name, pat_secret);
-          if (sesh.authorized) {
-            // spread members of the Session "sesh"
+          // used for backend HTTP calls
+          const rest_scopes = [
+            "tableau:datasources:read",
+            "tableau:workbooks:read",
+            "tableau:projects:read",
+            "tableau:insight_definitions_metrics:read", 
+            "tableau:insight_metrics:read",
+            "tableau:metric_subscriptions:read",
+          ];
+          const rest_options = { 
+            jwt_secret: rest_secret, 
+            jwt_secret_id: rest_secret_id, 
+            jwt_client_id 
+          };
+          const rest_session = new Session(user.name);
+          await rest_session.jwt(user.email, rest_options, rest_scopes);
+          // await rest_session.pat(pat_name, pat_secret);
+
+          if (embed_session.authorized && rest_session.authorized) {
+            // frontend: user_id & embed_token
              const { 
               username, user_id, embed_token, site_id, site, created, expires,
-            } = sesh;
-            const { rest_key, user_id: rest_id } = rest_sesh;
+            } = embed_session;
+            // backend: rest_id & rest_key
+            const { user_id: rest_id, rest_key } = rest_session;
             // add members to a new tableau object in user
             user.tableau = {
-              username, user_id, embed_token, rest_key, rest_id, site_id, site, created, expires,
+              username, user_id, embed_token, rest_id, rest_key, site_id, site, created, expires,
             };
           }
-          return sesh.authorized && user.tableau.embed_token ? user : false; // Return false to display a default error message
+          // Return false to display a default error message
+          return user.tableau ? user : false; 
         } else {
           return false;
         }
