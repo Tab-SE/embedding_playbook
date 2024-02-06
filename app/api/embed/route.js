882f68830e899710b15b4bfde9936aa35d6a5cd2
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-05 22:07:30 -0600

    embed route

--- app/api/embed/route.js
+++ app/api/embed/route.js
@@ -2,10 +2,9 @@ export const dynamic = 'force-dynamic'; // static by default, unless reading the
 
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-import { makePayload } from './methods';
 
-// responds with a full set of metrics data for generating Pulse Insights
-export async function GET(req) {
+// local connected app
+export async function POST(req) {
   // Check if req is defined
   if (!req) {
     return NextResponse.json({ error: '400: Bad Request' }, { status: 400 });
@@ -15,9 +14,10 @@ export async function GET(req) {
 
   // Check if token is defined
   if (token?.tableau) {
-    const payload = await makePayload(token.tableau);
     if (payload) {
-      return NextResponse.json(payload, { status: 200 });
+      const embed = token.tableau;
+      console.log(embed);
+      return NextResponse.json(embed, { status: 200 });
     } else {
       return NextResponse.json({ error: '500: Internal error: cannot generate payload' }, { status: 500 });
     }
@@ -26,3 +26,4 @@ export async function GET(req) {
     return NextResponse.json({ error: '401: Unauthorized' }, { status: 401 });
   }
 }
+
