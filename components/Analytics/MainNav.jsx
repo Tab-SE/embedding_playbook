ccd453df2e27fe805cc2b1d50b3dc7fb942ff5ba
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-12 15:28:40 -0600

    spacing out theme selector and avatar

--- components/Analytics/MainNav.jsx
+++ components/Analytics/MainNav.jsx
@@ -1,16 +1,17 @@
-import { Workbooks } from '../Workbooks';
 import { ThemeSelect } from '../ThemeSelect';
 import { UserNav } from '../UserNav';
 
 export const MainNav = () => {
-
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4">
-        <Workbooks className="mx-6" />
-        <div className="ml-auto flex items-center space-x-4">
-          <ThemeSelect />
-          <UserNav />
+      <div className="h-16 px-4">
+        <div className="flex items-center justify-between space-x-4">
+          <div>
+            <ThemeSelect />
+          </div>
+          <div>
+            <UserNav />
+          </div>
         </div>
       </div>
     </div>
