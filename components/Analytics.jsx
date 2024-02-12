777db375297fa076762586bc1d638ccb461c92f6
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-12 00:27:26 -0600

    testing components

--- components/Analytics.jsx
+++ components/Analytics.jsx
@@ -1,14 +1,42 @@
 import { useState, useRef } from 'react';
 
+import { 
+  Calendar,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger, 
+} from './ui';
+
+
 export const Analytics = (props) => {
   const { src, height, width, hideTabs, device, toolbar } = props;
   // useRef accesses DOM nodes created with the render method https://reactjs.org/docs/refs-and-the-dom.html
   const ref = useRef(null); 
   const [interactive, setInteractive] = useState(false);
+  const [date, setDate] = useState(new Date());
 
   return (
     <section className='bg-colorblind-lgray rounded pb-3'> 
-
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </section>
   )
 }
