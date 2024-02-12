24b16aeb9c38a33b5e15c50a2e29043c14c0df28
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-11 22:55:23 -0600

    removing daisyui config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const daisyUI = require("daisyui");
 
 module.exports = {
   content: [
@@ -41,37 +40,5 @@ module.exports = {
       }
     },
   },
-  daisyui: {
-    themes: [
-      {
-        light: {
-          "primary": "#0d9dda",       
-          "secondary": "#ff5d2d",     
-          "accent": "#04e1cb",  
-          "neutral": "#444444",    
-          "base-100": "#ffffff",    
-          "info": "#0176d3",       
-          "success": "#41b658",        
-          "warning": "#fcc003",     
-          "error": "#fe5c4c",
-        },
-        dark: {
-          "primary": "#0d9dda",       
-          "secondary": "#ff5d2d",     
-          "accent": "#04e1cb",  
-          "neutral": "#444444",    
-          "base-100": "#ffffff",    
-          "info": "#0176d3",       
-          "success": "#41b658",        
-          "warning": "#fcc003",     
-          "error": "#fe5c4c",
-          "backgroundColor": "rgba(17,17,17,var(--tw-bg-opacity))",
-        },
-      },
-    ],
-  },
-  plugins: [
-    daisyUI,
-  ],
 }
 
