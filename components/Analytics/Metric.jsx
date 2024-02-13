ae06270ec99d222c0b6111f0e9facd03da033c5a
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-13 00:39:25 -0600

    remove SVG

--- components/Analytics/Metric.jsx
+++ components/Analytics/Metric.jsx
@@ -73,18 +73,6 @@ export const Metric = (props) => {
           <CardTitle className="text-sm font-medium">
             {metric.name}
           </CardTitle>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="h-4 w-4 text-muted-foreground"
-          >
-            <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-          </svg>
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">{stats.value ? stats.value : null}</div>
