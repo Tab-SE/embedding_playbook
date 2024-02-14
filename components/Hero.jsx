b8e33580122f2cf464306be8b9a486ce35ff2598
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-13 23:02:03 -0600

    reduced top margin

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = (props) => {
   const { children, src, width, height, hideTabs, device, toolbar } = props;
 
   return (
-    <div className='mt-12 grid grid-cols-1 md:grid-cols-12 md:gap-9'>
+    <div className='mt-9 grid grid-cols-1 md:grid-cols-12 md:gap-9'>
       <div className='col-span-4'>
         {children}
       </div>
