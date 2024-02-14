da8f5845c29b4a3138aa2d1ea70bf17d952a8421
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-13 19:48:54 -0600

    dark theme borders corrected

--- components/Analytics/Analytics.jsx
+++ components/Analytics/Analytics.jsx
@@ -1,8 +1,5 @@
 import { useState, useRef } from 'react';
 
-import { Button } from "../ui";
-
-// import { CalendarDateRangePicker } from "@/app/examples/dashboard/components/date-range-picker"
 import { MobilePreview } from './MobilePreview';
 import { MainNav } from './MainNav';
 import { Metrics } from './Metrics';
@@ -20,19 +17,12 @@ export const Analytics = (props) => {
     <>
       <MobilePreview />
       <div className="hidden flex-col md:flex">
-        <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-xl">
+        <div className="overflow-hidden rounded-[0.5rem] border dark:border-stone-600 bg-background shadow-md md:shadow-xl">
           <MainNav />
           <div className="flex-1 space-y-4 p-8 pt-6">
-            <div className="flex items-center justify-between space-y-2">
-              <h2 className="text-3xl font-bold tracking-tight">Superstore Analytics</h2>
-              <div className="flex items-center space-x-2">
-                {/* <CalendarDateRangePicker /> */}
-                <Button>Download</Button>
-              </div>
-            </div>
             <Metrics />
             <Sheets />
-        </div>
+          </div>
         </div>
       </div>
     </>
--- components/Analytics/MainNav.jsx
+++ components/Analytics/MainNav.jsx
@@ -1,9 +1,9 @@
-import { ThemeSelect } from '../ThemeSelect';
+import { ThemeSelect } from './ThemeSelect';
 import { UserMenu } from './UserMenu';
 
 export const MainNav = () => {
   return (
-    <div className="border-b">
+    <div className="border-b dark:border-stone-600">
       <div className="h-18 px-4 mb-2">
         <div className="flex items-center justify-between space-x-4 mt-3">
           <ThemeSelect />
