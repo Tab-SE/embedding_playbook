7ab6a5f1cc5c0ea40e64c0f2061781e7c4a0911d
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-14 12:01:08 -0600

    more x padding

--- components/Analytics/MainNav.jsx
+++ components/Analytics/MainNav.jsx
@@ -3,7 +3,7 @@ import { UserMenu } from './UserMenu';
 
 export const MainNav = () => {
   return (
-    <div className="border-b dark:border-stone-600 bg-stone-300 dark:bg-stone-800 shadow-xl">
+    <div className="border-b dark:border-stone-600 bg-stone-300 dark:bg-stone-800 shadow-xl px-3">
       <div className="h-18 px-4 mb-2">
         <div className="flex items-center justify-between space-x-4 pt-3">
           <ThemeSelect />
