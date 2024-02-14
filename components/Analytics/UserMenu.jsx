b8643bfc759465aeaecd9ed4704958f23ca03ad2
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-13 22:36:36 -0600

    can refresh session on log out

--- components/Analytics/UserMenu.jsx
+++ components/Analytics/UserMenu.jsx
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useState, useEffect } from "react";
 
 import {
   Avatar,
@@ -17,52 +18,115 @@ import {
   DropdownMenuTrigger,
 } from "../ui";
 
+
 export function UserMenu(props) {
   const { src } = props;
   const avatar = src ? src : 'img/users/mackenzie_day.png';
+  const [user, setUser] = useState(undefined);
+  // only 2 states: loading and authenticated https://next-auth.js.org/getting-started/client#require-session
+  const { status, data } = useSession({ required: false });
 
+  useEffect(() => {
+    if (status === 'authenticated') {
+      setUser(data.user.name);
+    }
+  }, [status, data]);
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-12 w-12 rounded-full">
-          <Avatar className="h-12 w-12">
-            <AvatarImage src={avatar} alt="@shadcn" />
-            <AvatarFallback>MD</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
+      <Trigger src={avatar} />
       <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Superstore Analytics</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              mday@mail.com
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Profile
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Billing
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>New Team</DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
+        <Label />
+        <Group />
+        <Logout status={status} />
       </DropdownMenuContent>
     </DropdownMenu>
+  )
+}
+
+
+const Logout = (props) => {
+  const { status } = props;
+
+  if (status === 'authenticated') {
+    return (
+      <DropdownMenuItem>
+        <Button 
+          variant="ghost" 
+          className="h-min p-1" 
+          onClick={async () => await signOut('credentials', { redirect: false })}
+        >
+        Log out
+        </Button>
+        <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+      </DropdownMenuItem>
+    )
+  }
+
+  return (
+    <DropdownMenuItem>
+      <Button 
+        variant="ghost" 
+        className="h-min p-1" 
+        onClick={async () => await signIn()}
+      >
+      Log in
+      </Button>
+      <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+    </DropdownMenuItem>
+  )
+}
+
+
+const Trigger = (props) => {
+  const { src } = props;
+  return (
+    <DropdownMenuTrigger asChild>
+      <Button variant="ghost" className="relative h-12 w-12 rounded-full">
+        <Avatar className="h-12 w-12">
+          <AvatarImage src={src} alt="user profile picture" />
+          <AvatarFallback>MD</AvatarFallback>
+        </Avatar>
+      </Button>
+    </DropdownMenuTrigger>
+  )
+}
+
+
+const Label = (props) => {
+  return (
+    <DropdownMenuLabel className="font-normal">
+      <div className="flex flex-col space-y-1">
+        <p className="text-sm font-medium leading-none">Superstore Analytics</p>
+        <p className="text-xs leading-none text-muted-foreground">
+          mday@mail.com
+        </p>
+      </div>
+    </DropdownMenuLabel>
+  )
+}
+
+
+const Group = (props) => {
+  return (
+    <div>
+      <DropdownMenuSeparator />
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          Profile
+          <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          Billing
+          <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          Settings
+          <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuItem>New Team</DropdownMenuItem>
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+    </div>
   )
 }
