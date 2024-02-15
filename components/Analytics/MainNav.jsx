494512a139ff291d676d04f856b70891ff46c453
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-14 21:56:02 -0600

    darker light theme backgrounds

--- components/Analytics/MainNav.jsx
+++ components/Analytics/MainNav.jsx
@@ -3,7 +3,7 @@ import { UserMenu } from './UserMenu';
 
 export const MainNav = () => {
   return (
-    <div className="border-b dark:border-stone-600 bg-stone-400 dark:bg-stone-900 shadow-xl px-3">
+    <div className="border-b dark:border-stone-600 bg-stone-600 dark:bg-stone-900 shadow-xl px-3">
       <div className="h-18 px-4 mb-2">
         <div className="flex items-center justify-between space-x-4 pt-3">
           <ThemeSelect />
