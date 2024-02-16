29a9e2ab45d0d0deb50721047f9383ba659aa1ec
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-16 01:17:42 -0600

    fixing title tags

--- components/Head.jsx
+++ components/Head.jsx
@@ -2,6 +2,10 @@ export const Head = (props) => {
   const { imgHost, title, description } = props;
   const previewImage = imgHost + "/img/opengraph/embed_tableau.png";
   const url = 'https://embedding-playbook.vercel.app';
+  let formattedTitle = title;
+  if (title === 'Index') {
+    formattedTitle = 'Embed Tableau ✨';
+  }
 
   return (
     <>
@@ -12,19 +16,19 @@ export const Head = (props) => {
       <meta httpEquiv="Content-Language" content="en" />
       <meta name="description" content={description} />
 
-      <meta name="og:title" content={title} />
+      <meta name="og:title" content={formattedTitle} />
       <meta name="og:image" content={previewImage} alt="embed tableau preview" />
       <meta name="og:description" content={description} />
       <meta name="og:url" content={url} />
 
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={formattedTitle} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:card" content={previewImage} />
       <meta name="twitter:image" content={previewImage} alt="embed tableau preview" />
       <meta name="twitter:site:domain" content={url} />
       <meta name="twitter:url" content={url} />
       
-      <meta name="apple-mobile-web-app-title" content={title} />
+      <meta name="apple-mobile-web-app-title" content={formattedTitle} />
 
       <link rel="canonical" href={url} />
       <link rel="icon" href={imgHost + "/svg/logo_color.svg?h=32&w=32"} type="image/svg+xml" />
