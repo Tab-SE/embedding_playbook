71452f3b869ebe67dd3b0590945862eeed99d48c
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-15 23:23:50 -0600

    working theme icons

--- components/Analytics/ThemeSelect.jsx
+++ components/Analytics/ThemeSelect.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import {
   CaretSortIcon,
   CheckIcon,
-  PlusCircledIcon,
 } from "@radix-ui/react-icons";
 
 import { cn } from "utils";
@@ -22,27 +21,9 @@ import {
   CommandSeparator,
 } from "components/ui";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "components/ui";
-import { Input } from "components/ui";
-import { Label } from "components/ui";
-import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "components/ui";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
 } from "components/ui";
 
 const groups = [
@@ -62,10 +43,6 @@ const groups = [
         label: "Northern Trail Outfitters",
         value: "nto",
       },
-      {
-        label: "E-Bike",
-        value: "monsters",
-      },
     ],
   },
 ];
@@ -74,7 +51,6 @@ const groups = [
 export const ThemeSelect = (props) => {
   const { className, setTheme } = props;
   const [open, setOpen] = React.useState(false);
-  const [showNewTeamDialog, setShowNewTeamDialog] = React.useState(false);
   const [selectedTeam, setSelectedTeam] = React.useState(
     groups[0].teams[0]
   );
@@ -85,129 +61,66 @@ export const ThemeSelect = (props) => {
 
 
   return (
-    <Dialog open={showNewTeamDialog} onOpenChange={setShowNewTeamDialog}>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            aria-label="Select a team"
-            className={cn("justify-between text-lg md:text-xl font-bold tracking-tight p-6 dark:bg-stone-900", className)}
-          >
-            <Avatar className="mr-2 h-5 w-5">
-              <AvatarImage
-                src={`https://avatar.vercel.sh/${selectedTeam.value}.png`}
-                alt={selectedTeam.label}
-                className="grayscale"
-              />
-              <AvatarFallback>SC</AvatarFallback>
-            </Avatar>
-            {selectedTeam.label}
-            <CaretSortIcon className="ml-3 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
-          <Command>
-            <CommandList className="dark:bg-stone-700">
-              <CommandInput placeholder="Search team..." />
-              <CommandEmpty>No team found.</CommandEmpty>
-              {groups.map((group) => (
-                <CommandGroup key={group.label} heading={group.label}>
-                  {group.teams.map((team) => (
-                    <CommandItem
-                      key={team.value}
-                      onSelect={() => {
-                        setSelectedTeam(team)
-                        setOpen(false)
-                      }}
-                      className="text-sm"
-                    >
-                      <Avatar className="mr-2 h-5 w-5">
-                        <AvatarImage
-                          src={`https://avatar.vercel.sh/${team.value}.png`}
-                          alt={team.label}
-                          className="grayscale"
-                        />
-                        <AvatarFallback>SC</AvatarFallback>
-                      </Avatar>
-                      {team.label}
-                      <CheckIcon
-                        className={cn(
-                          "ml-auto h-4 w-4",
-                          selectedTeam.value === team.value
-                            ? "opacity-100"
-                            : "opacity-0"
-                        )}
-                      />
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              ))}
-            </CommandList>
-            <CommandSeparator />
-            <CommandList>
-              <CommandGroup className="dark:bg-stone-700">
-                <DialogTrigger asChild>
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          aria-label="Select a team"
+          className={cn("justify-between text-lg md:text-xl font-bold tracking-tight p-6 dark:bg-stone-900", className)}
+        >
+          <Avatar className="mr-2 h-5 w-5">
+            <AvatarImage
+              src={`img/themes/${selectedTeam.value}.png`}
+              alt={selectedTeam.label}
+            />
+            <AvatarFallback>&nbsp;</AvatarFallback>
+          </Avatar>
+          {selectedTeam.label}
+          <CaretSortIcon className="ml-3 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList className="dark:bg-stone-700">
+            <CommandInput placeholder="Search team..." />
+            <CommandEmpty>No team found.</CommandEmpty>
+            {groups.map((group) => (
+              <CommandGroup key={group.label} heading={group.label}>
+                {group.teams.map((team) => (
                   <CommandItem
+                    key={team.value}
                     onSelect={() => {
+                      setSelectedTeam(team)
                       setOpen(false)
-                      setShowNewTeamDialog(true)
                     }}
+                    className="text-sm"
                   >
-                    <PlusCircledIcon className="mr-2 h-5 w-5" />
-                    Create Team
+                    <Avatar className="mr-2 h-5 w-5">
+                      <AvatarImage
+                        src={`img/themes/${team.value}.png`}
+                        alt={team.label}
+                      />
+                      <AvatarFallback>&nbsp;</AvatarFallback>
+                    </Avatar>
+                    {team.label}
+                    <CheckIcon
+                      className={cn(
+                        "ml-auto h-4 w-4",
+                        selectedTeam.value === team.value
+                          ? "opacity-100"
+                          : "opacity-0"
+                      )}
+                    />
                   </CommandItem>
-                </DialogTrigger>
+                ))}
               </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Create team</DialogTitle>
-          <DialogDescription>
-            Add a new team to manage products and customers.
-          </DialogDescription>
-        </DialogHeader>
-        <div>
-          <div className="space-y-4 py-2 pb-4">
-            <div className="space-y-2">
-              <Label htmlFor="name">Team name</Label>
-              <Input id="name" placeholder="Acme Inc." />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="plan">Subscription plan</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a plan" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="free">
-                    <span className="font-medium">Free</span> -{" "}
-                    <span className="text-muted-foreground">
-                      Trial for two weeks
-                    </span>
-                  </SelectItem>
-                  <SelectItem value="pro">
-                    <span className="font-medium">Pro</span> -{" "}
-                    <span className="text-muted-foreground">
-                      $9/month per user
-                    </span>
-                  </SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </div>
-        <DialogFooter>
-          <Button variant="outline" onClick={() => setShowNewTeamDialog(false)}>
-            Cancel
-          </Button>
-          <Button type="submit">Continue</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+            ))}
+          </CommandList>
+          <CommandSeparator />
+        </Command>
+      </PopoverContent>
+    </Popover>
   )
 }
