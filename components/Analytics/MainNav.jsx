15fc85a8c5875708c12c60cc59ad03f587bf756d
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-15 23:48:14 -0600

    window icons

--- components/Analytics/MainNav.jsx
+++ components/Analytics/MainNav.jsx
@@ -1,3 +1,5 @@
+import { Button } from "components/ui/";
+
 import { ThemeSelect, UserMenu } from './index';
 
 
@@ -6,6 +8,11 @@ export const MainNav = (props) => {
 
   return (
     <div className="border-b dark:border-stone-600 bg-stone-600 dark:bg-stone-900 shadow-xl px-3">
+      <div className="h-3 mb-2">
+        <Button className="h-3 w-3 text-[6px] bg-red-500 dark:bg-red-500 p-1 mx-1 rounded-full">✕</Button>
+        <Button className="h-3 w-3 text-[6px] bg-yellow-400 dark:bg-yellow-500 p-1 mx-1 rounded-full">─</Button>
+        <Button className="h-3 w-3 text-[6px] bg-green-500 dark:bg-green-500 p-1 mx-1 rounded-full">☐</Button>
+      </div>
       <div className="h-18 px-4 mb-2">
         <div className="flex items-center justify-between space-x-4 pt-3">
           <ThemeSelect setTheme={setTheme} />
