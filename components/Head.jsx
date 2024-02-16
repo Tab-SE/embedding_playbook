fdd780eb60c28d6bec5256dbea31d99932519160
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-16 01:12:32 -0600

    adding missing tags to Head

--- components/Head.jsx
+++ components/Head.jsx
@@ -1,37 +1,44 @@
 export const Head = (props) => {
+  const { imgHost, title, description } = props;
+  const previewImage = imgHost + "/img/opengraph/embed_tableau.png";
+  const url = 'https://embedding-playbook.vercel.app';
+
   return (
     <>
+      <meta name="robots" content="index, follow, archive, snippet" />
       <meta name="msapplication-TileColor" content="#fff" />
       <meta name="theme-color" content="#fff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta
-        name="description"
-        content={props.description}
-      />
-      <meta
-        name="og:description"
-        content={props.description}
-      />
-      <meta name="twitter:card" content={props.imgHost + "/img/tableau/logo_text.png?w=350"} />
-      <meta name="twitter:image" content={props.socialCard} />
-      <meta name="twitter:site:domain" content="https://tab-se.github.io" />
-      <meta name="twitter:url" content="https://tab-se.github.io/embedding_playbook" />
-      <meta name="og:title" content={props.title}/>
-      <meta name="og:image" content={props.socialCard} />
-      <meta name="apple-mobile-web-app-title" content={props.title} />
-      <link rel="icon" href={props.imgHost + "/svg/logo_color.svg?h=32&w=32"} type="image/svg+xml" />
-      <link rel="icon" href={props.imgHost + "/img/tableau/tableau.ico?h=32&w=32"} type="image/ico" />
-      <link rel="icon" href={props.imgHost + "/img.tableau/tableau_logo.png?h=32&w=32"} type="image/png" />
+      <meta name="description" content={description} />
+
+      <meta name="og:title" content={title} />
+      <meta name="og:image" content={previewImage} alt="embed tableau preview" />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:card" content={previewImage} />
+      <meta name="twitter:image" content={previewImage} alt="embed tableau preview" />
+      <meta name="twitter:site:domain" content={url} />
+      <meta name="twitter:url" content={url} />
+      
+      <meta name="apple-mobile-web-app-title" content={title} />
+
+      <link rel="canonical" href={url} />
+      <link rel="icon" href={imgHost + "/svg/logo_color.svg?h=32&w=32"} type="image/svg+xml" />
+      <link rel="icon" href={imgHost + "/img/tableau/tableau.ico?h=32&w=32"} type="image/ico" />
+      <link rel="icon" href={imgHost + "/img.tableau/tableau_logo.png?h=32&w=32"} type="image/png" />
       <link
         rel="icon"
-        href={props.imgHost + "/svg/dark.svg?h=32&w=32"}
+        href={imgHost + "/svg/dark.svg?h=32&w=32"}
         type="image/svg+xml"
         media="(prefers-color-scheme: dark)"
       />
       <link
         rel="icon"
-        href={props.imgHost + "/img/tableau/tableau_logo_dark.png?h=32&w=32"}
+        href={imgHost + "/img/tableau/tableau_logo_dark.png?h=32&w=32"}
         type="image/png"
         media="(prefers-color-scheme: dark)"
       />
