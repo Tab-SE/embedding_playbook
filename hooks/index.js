27ac0c9a0623ae3153d321699d1dec901ae581d5
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-25 19:33:25 -0600

    removing unused hooks

--- hooks/index.js
+++ hooks/index.js
@@ -1,7 +1,4 @@
 export { useTableauSession } from './useTableauSession';
 export { useMetrics } from './useMetrics';
 export { useInsights } from './useInsights';
-export { useBan } from './useBan';
-export { useSpringboard } from './useSpringboard';
-export { useDetail } from './useDetail';
 export { useTimeout } from './useTimeout';
