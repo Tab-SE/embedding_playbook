81a78075ba1b645fbace2117748aca14c559a853
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-28 11:25:56 -0600

    created useMetadata hook

--- hooks/useMetadata.js
+++ hooks/useMetadata.js
@@ -1,18 +1,19 @@
-import { useQuery } from "@tanstack/react-query"
-import { getMetrics } from "libs"
+import { useQuery } from "@tanstack/react-query";
+
+import { getMetadata } from "libs";
 // implements custom hooks with tanstack query for asynchronous state management
 // concepts described here: https://tkdodo.eu/blog/react-query-as-a-state-manager
 // more on query key structure: https://tkdodo.eu/blog/effective-react-query-keys#structure
 // more on dependent queries: https://tanstack.com/query/v3/docs/react/guides/dependent-queries
 
-export const useMetrics = (user) => {
+export const useMetadata = (user) => {
   // set to an empty array if enumerated function parameters are not available in array
-  const queryKey = [user].every(param => param != null) ? ["tableau", "metrics", user] : []; 
+  const queryKey = [user].every(param => param != null) ? ["tableau", "metadata", user] : []; 
 
   return useQuery({
     queryKey: queryKey, 
     queryFn: () => {
-      return getMetrics();
+      return getMetadata();
     },
     enabled: !!user,
     retry: 3,
