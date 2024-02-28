03d66fd74a79448e70fd3e8f39e2b1c1b6d0ee57
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-02-28 11:03:16 -0600

    importing gql methods

--- app/api/ams/methods.js
+++ app/api/ams/methods.js
@@ -1,11 +1,10 @@
-
+import { queryMetadata } from 'libs';
 
 // obtains metadata from Tableau's GraphQL endpoint
 export const getMetadata = async (tableau) => {
-  const { rest_id, rest_key } = tableau;
-  if (rest_id && rest_key) {
-    // new Metrics model with data obtained using temporary key
-    const payload = await queryMetadata(rest_id, rest_key); 
+  const { rest_key } = tableau;
+  if (rest_key) {
+    const payload = await queryMetadata(rest_key); 
     return payload;
   } else {
     // errors resolve to false when checked
