589c6a3ed398a6315331ae1415c863462b844df6
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-03-11 22:27:01 -0500

    base pulse component with session security

---
+++ components/Pulse.jsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+import { tab_embed } from 'libs';
+import { useTableauSession } from 'hooks';
+
+export const Pulse = (props) => {
+  const { src, height, width, disableExploreFilter } = props;
+
+  // tanstack query hook to manage embed sessions
+  const { status, data: jwt, error, isSuccess, isError, isLoading } = useTableauSession('a');
+
+  if (isError) {
+    console.debug(error);
+  }
+
+  <div className='rounded'>
+      {isError ? <p>Authentication Error!</p> : null}
+      {isLoading ? <p>Authenticating the User...</p> : null}
+      {isSuccess ? 
+        <tableau-pulse 
+          id="tableauPulse"
+          src={src}
+          height={height}
+          width={width}
+          token={jwt}
+          disableExploreFilter={disableExploreFilter}
+        /> : null}
+    </div>
+}
