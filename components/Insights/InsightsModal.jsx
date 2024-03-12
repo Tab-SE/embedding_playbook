57c2e25763bd0d66d1fdaea11783147e495402b5
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-03-11 23:06:11 -0500

    adding tabs to show pulse or carrousel

--- components/Insights/InsightsModal.jsx
+++ components/Insights/InsightsModal.jsx
@@ -1,26 +1,49 @@
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "components/ui";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "components/ui";
 
-import { Insights } from "components";
+import { Insights, Pulse } from "components";
 
 export const InsightsModal = (props) => {
   const { metric, stats } = props;
+  const domain = process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN;
+  const site = process.env.NEXT_PUBLIC_ANALYTICS_SITE;
+  const src = `${domain}/site/${site}/pulse/metrics/${metric.id}`;
 
   return (
-    <DialogContent className="max-w-full w-max dark:bg-stone-900">
-      <DialogHeader className="ml-6">
-        <DialogTitle className="text-3xl">
-          <span className="text-stone-600 dark:text-stone-300">{metric.name}:</span> <span className="font-bold ml-3">{stats.value}</span>
-        </DialogTitle>
-        <DialogDescription>
-          <span className={`text-2xl text-muted-foreground ${stats.color}`}>
-            {stats.direction} {stats.absolute}
-          </span>
-          <span className={`text-2xl text-muted-foreground ${stats.color}`}>
-            &nbsp; △ {stats.relative ? `${stats.relative}` : null}
-          </span>
-        </DialogDescription>
-      </DialogHeader>
-      <Insights metric={metric} stats={stats} />
+    <DialogContent className="max-w-full w-[1250px] h-[700px] dark:bg-stone-900">
+      <Tabs defaultValue="pulse" className="">
+        <TabsList className="mb-6">
+          <TabsTrigger value="pulse">Pulse Metric</TabsTrigger>
+          <TabsTrigger value="insights">AI Insights</TabsTrigger>
+        </TabsList>
+        <TabsContent value="pulse">
+            <Pulse 
+              src={src}
+              height='600px'
+              width='1000px'
+            />
+        </TabsContent>
+
+        <TabsContent value="insights">
+          <DialogHeader className="ml-6 mb-6">
+            <DialogTitle className="text-3xl">
+              <span className="text-stone-600 dark:text-stone-300">{metric.name}:</span> <span className="font-bold ml-3">{stats.value}</span>
+            </DialogTitle>
+            <DialogDescription>
+              <span className={`text-2xl text-muted-foreground ${stats.color}`}>
+                {stats.direction} {stats.absolute}
+              </span>
+              <span className={`text-2xl text-muted-foreground ${stats.color}`}>
+                &nbsp; △ {stats.relative ? `${stats.relative}` : null}
+              </span>
+            </DialogDescription>
+          </DialogHeader>
+          <Insights metric={metric} stats={stats} />
+        </TabsContent>
+        
+      </Tabs>
     </DialogContent>
   )
 }
+
+
