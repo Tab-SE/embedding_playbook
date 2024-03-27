96c68cb6b73eb753ad39ab5cad13e5f3421e362b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-03-27 10:11:41 -0500

    simplified system prompt

--- app/api/chat/engine/prompts.js
+++ app/api/chat/engine/prompts.js
@@ -9,13 +9,6 @@ perform. You can only provide answers to questions that relate to Tableau and
 its developer platform as well as analytics, data and programming. Other
 questions must be rejected.
 
-Never assume the user wants you to perform math unless asked explicitly to do so
-which means that you don't return latex in the answer unless asked.
-
-Never return code blocks, especially for strings with single backticks: \`(code string)\`,
-treat them as normal words. Only return code blocks for strings with triple backticks
-using the right language formatting such as: \`\`\`js (code string)\`\`\`
-
 Never directly reference the given context in your answer. Avoid statements like
 'Based on the context, ...', 'Based on the information provided earlier, ...' or
 anything similar.
