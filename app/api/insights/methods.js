3a6cd68df6daf1b3d2e06f23e1a32f86f758e5e1
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-04-16 00:44:11 -0500

    new format for now

--- app/api/insights/methods.js
+++ app/api/insights/methods.js
@@ -48,25 +48,23 @@ const getInsightBundle = async (apiKey, metric, resource) => {
 const makeBundleBody = (metric) => {
   // calculate time for "now" value in required format
   const currentTime = new Date();
-  // Get the date components
-  const year = currentTime.getFullYear();
-  const month = (currentTime.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-  const day = currentTime.getDate().toString().padStart(2, '0');
-  const hour = currentTime.getHours().toString().padStart(2, '0');
-  const minute = currentTime.getMinutes().toString().padStart(2, '0');
-  const second = currentTime.getSeconds().toString().padStart(2, '0');
-  // Format the date as "YYYY-MM-DD HH:mm:ss"
-  const formattedDate = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+
+  // Format the date and time to 'YYYY-MM-DD HH:MM:SS'
+  const formattedDate = currentTime.getFullYear() +
+    "-" + ("0" + (currentTime.getMonth() + 1)).slice(-2) +
+    "-" + ("0" + currentTime.getDate()).slice(-2) +
+    " " + ("0" + currentTime.getHours()).slice(-2) +
+    ":" + ("0" + currentTime.getMinutes()).slice(-2) +
+    ":" + ("0" + currentTime.getSeconds()).slice(-2);
 
   // Get the time zone
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   // all other object members
-  const { 
-    name, id, specification_id, definition, specification, 
+  const {
+    name, id, specification_id, definition, specification,
     extension_options, representation_options, insights_options,
-   } = metric;
-
+  } = metric;
 
   const body = {
     bundle_request: {
