5a3f37cd23904b8070dbf1164e78e8dccaa21af6
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-04-28 11:58:53 -0500

    stubs for web component

---
+++ components/TableauWebAuthor/TableauWebAuthor.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import { tab_embed } from 'libs';
+
+
+// handles post authentication logic requiring an initialized <tableau-authoring-viz> object to operate
+export const TableauWebAuthor = forwardRef(function Viz(props, ref) {
+  const { } = props;
+
+  return (
+    <></>
+  )
+})
---
+++ components/TableauWebAuthor/index.js
@@ -0,0 +1,1 @@
+export { TableauWebAuthor } from './TableauWebAuthor';
