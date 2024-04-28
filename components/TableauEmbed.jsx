2e1eca8ddcd710a82f1bbc07c4923901830cabb5
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-04-28 11:58:20 -0500

    HOC to share auth handling for all embeds

---
+++ components/TableauEmbed.jsx
@@ -0,0 +1,82 @@
+// eslint-disable-next-line no-unused-vars
+import { tab_embed } from 'libs';
+import { useEffect, useState, useRef, forwardRef, useId } from 'react';
+
+import { useTableauSession } from 'hooks';
+import { TableauViz, TableauWebAuthor } from 'components';
+
+// forwardRef HOC receives ref from parent and sets placeholder
+export const TableauEmbed = forwardRef(function TableauViz(props, ref) {
+  const { src, height, width, device, hideTabs, toolbar, isPublic } = props;
+
+  // size of parent div placeholder
+  let containerHeight = height;
+  let containerWidth = width;
+  if (toolbar === 'hidden') {
+    containerHeight = height;
+  }
+  const containerStyle = {
+    height: containerHeight + 'px',
+    width: containerWidth + 'px',
+  };
+
+  return (
+    <div
+      className='rounded'
+      style={containerStyle}
+    >
+      <AuthLayer
+        src={src}
+        ref={ref}
+        height={height}
+        width={width}
+        device={device}
+        hide-tabs={hideTabs ? true : false}
+        toolbar={toolbar}
+        isPublic={isPublic}
+      />
+    </div>
+  )
+});
+
+// handles rendering logic during authentication
+const AuthLayer = forwardRef(function AuthLayer(props, ref) {
+  const { src, height, width, device, hideTabs, toolbar, isPublic } = props;
+
+  // tanstack query hook to manage embed sessions
+  const {
+    status,
+    data: jwt,
+    error: sessionError,
+    isSuccess: isSessionSuccess,
+    isError: isSessionError,
+    isLoading: isSessionLoading
+  } = useTableauSession('a');
+
+  if (isSessionError) {
+    console.debug(sessionError);
+  }
+
+  if (isSessionSuccess) {
+  }
+
+  return (
+    <div className='rounded'>
+      {isSessionError ? <p>Authentication Error!</p> : null}
+      {isSessionLoading ? <p>Authenticating the User...</p> : null}
+      {isSessionSuccess ?
+        <TableauViz
+          src={src}
+          ref={ref}
+          jwt={jwt}
+          height={height}
+          width={width}
+          device={device}
+          hide-tabs={hideTabs ? true : false}
+          toolbar={toolbar}
+          isPublic={isPublic}
+          class=''
+        /> : null}
+    </div>
+  )
+})
