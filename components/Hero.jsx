181147cdb2149a0a4b1fa453e0e90b251b10337d
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-05-07 15:59:03 -0500

    equity tab

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -63,7 +63,7 @@ const Intro = () => {
           is your trusted partner in navigating the dynamic landscape of the modern workplace.
           </p>
           <Image
-            src='img/themes/pacifica/pacifica_proserve.png'
+            src='https://plus.unsplash.com/premium_photo-1661901829247-0358bd917858?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             width='750'
             height='500'
             alt='stock pacifica photo'
@@ -87,7 +87,7 @@ const Outro = (props) => {
         navigate the complexities of the modern workplace with confidence and precision.
         </p>
         <Image
-          src='img/themes/pacifica/pacifica_staffing.png'
+          src='https://images.unsplash.com/photo-1556484687-30636164638b?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           width='750'
           height='500'
           alt='stock pacifica photo'
--- components/Themes/Pacifica/index.js
+++ components/Themes/Pacifica/index.js
@@ -1,20 +1,32 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "components/ui";
 
 import { DEI } from './DEI';
+import { Diversity } from './Diversity';
+import { Equity } from './Equity';
+
+
 
 export const Pacifica = () => {
   return (
     <Tabs defaultValue="dei" className="space-y-3">
       <TabsList>
         <TabsTrigger value="dei">
-          DEI
+          Summary
         </TabsTrigger>
-        <TabsTrigger value="gender">
-          Gender
+        <TabsTrigger value="diversity">
+          Diversity
+        </TabsTrigger>
+        <TabsTrigger value="equity">
+          Equity
+        </TabsTrigger>
+        <TabsTrigger value="retention">
+          Retention
         </TabsTrigger>
       </TabsList>
       <section className="min-h-[892px]">
         <DEI />
+        <Diversity />
+        <Equity />
       </section>
     </Tabs>
   )
