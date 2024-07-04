605ee0cd15a4b9cb2d576c06b5729991c83266e2
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-03 21:40:44 -0600

    adding code snippet to hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,6 +6,69 @@ import { Button } from 'components/ui';
 import { DisplayContextProvider } from 'components/context';
 import { Demo, Logo } from 'components';
 
+const Intro = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold mb-9'>Embed Tableau</h1>
+        <div className='leading-loose space-y-6'>
+          <p>Tableau is the world's leading end-to-end data and analytics platform.</p>
+          <p>
+            Leverage the analytical powerhouse of Tableau to analyze and visualize data.
+            The Embedding Playbook teaches you how to compose Tableau's varied product capabilities
+            into applications that thrill customers, coworkers, and friends!
+          </p>
+          <Image
+            src='/img/tableau/datadev.png'
+            width='750'
+            height='500'
+            alt='stock photo'
+          />
+        </div>
+    </div>
+  )
+}
+
+const Outro = (props) => {
+  const { snippet } = props;
+  return (
+    <div className='overflow-hidden'>
+      <p>
+        Beyond creating visual representations of data, Tableau provides
+        the greatest benefits as it helps people discover what information is valuable to others.
+        It enables users to analyze data and build the interfaces that represent them with unmatched speed
+        and flexibility resulting in a springboarding of ideas into value.
+      </p>
+      { snippet }
+    </div>
+  )
+}
+
+const HeroContent = (props) => {
+  const { hideMetrics, hideSheets, snippet } = props;
+
+  return (
+    <div className='grid grid-rows lg:grid-cols-12 space-y-6'>
+      <div className='lg:col-span-4 space-y-6 mr-6 mt-16'>
+        <Intro />
+        <div className='hidden lg:grid'>
+          <Outro snippet={snippet} />
+        </div>
+      </div>
+      <div className='lg:col-span-8'>
+        <Demo
+          hideMetrics={hideMetrics}
+          hideSheets={hideSheets}
+        />
+      </div>
+      <div className='grid lg:hidden'>
+        <Outro
+          snippet={snippet}
+        />
+      </div>
+    </div>
+  )
+}
+
 export const Hero = (props) => {
   const { children, hideMetrics, hideSheets } = props;
 
@@ -18,82 +81,4 @@ export const Hero = (props) => {
       />
     </DisplayContextProvider>
   );
-}
-
-const HeroContent = (props) => {
-  const { hideMetrics, hideSheets, snippet } = props;
-
-  return (
-    <Demo
-      hideMetrics={hideMetrics}
-      hideSheets={hideSheets}
-    />
-  )
-}
-
-const Intro = () => {
-  return (
-    <div className=''>
-      <Logo
-        src='/img/themes/zilliant/zilliant_light.svg'
-        width='420'
-        height='100'
-      />
-        <h3 className='italic text-2xl font-semibold mb-9 mt-6'>
-        TRANSFORM YOUR <span className='text-[#FD4926]'>PRICING</span> PROCESS
-        </h3>
-        <div className='leading-loose space-y-6 mt-6'>
-          <p>
-          Zilliant puts pricing at the heart of your business, transforming how you use data to price and sell.
-          See what the industry's leading cloud-native pricing, CPQ, and revenue optimization software can do for
-          your business.
-          </p>
-          <Button className="bg-[#FD4926] ml-12 mt-6">
-            <Link
-              href="/optimization"
-            >
-            Contact Us
-            </Link>
-          </Button>
-          <Image
-            src='https://zilliant.com/images/page-home/z-homepage-header-1365x800.webp'
-            width='750'
-            height='500'
-            alt='stock photo'
-          />
-          <p>
-          Zilliant helps businesses put pricing at the heart of their business by managing the entire pricing
-          lifecycle with leading CPQ, price management & optimzation, and Revenue Intelligence applications.
-          Zilliant's data science, cloud-native software and passion for customer success deliver the highest ROI,
-          fastest time to value and highest customer satisfaction.
-          </p>
-        </div>
-    </div>
-  )
-}
-
-const Outro = (props) => {
-  const { snippet } = props;
-  return (
-    <div className='overflow-hidden'>
-      <div className='leading-loose space-y-6 mt-6'>
-        <h4 className='font-semibold'>Upcoming Event</h4>
-        <h3 className='italic text-2xl font-semibold mb-9 mt-6'>
-          Zilliant <span className='text-[#FD4926]'>MindShare</span> 2024
-        </h3>
-        <Image
-          src='https://embed-ssl.wistia.com/deliveries/38ded08dd2ef59a38fb9ee9e74bb450c.webp?image_crop_resized=1920x1080'
-          width='750'
-          height='500'
-          alt='stock photo'
-        />
-        <p>
-        MindShare returns to Austin, Texas, on May 21 – 23, 2024, and will be our biggest yet!
-        The 3-day event will bring together pricing industry experts, visionaries, executives,
-        practitioners, and partners to share market trends, explore new ideas, and provide practical,
-        real-life stories on how to capture value across the entire pricing lifecycle.
-        </p>
-      </div>
-    </div>
-  )
 }
