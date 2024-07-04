43fe0998aa28278ab63ada3d2113f0c3f77b17cf
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-03 21:40:31 -0600

    defaulting to tableau logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,16 +1,20 @@
 import Image from 'next/image';
 
 export const Logo = (props) => {
-  const { src, width, height, alt } = props;
+  const { src, width, height, alt, text } = props;
 
   return (
-    <Image
-      src={src ? src : "/img/themes/zilliant/zilliant_light.svg"}
-      width={width ? width : "45"}
-      height={height ? height : "45"}
-      alt={alt ? alt : "application logo"}
-      placeholder="blur"
-      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
-    />
+    <>
+      <Image
+        src={src ? src : "/img/tableau/tableau_logo.png"}
+        width={width ? width : "45"}
+        height={height ? height : "45"}
+        alt={alt ? alt : "application logo"}
+        placeholder="blur"
+        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+      />
+      {text ? <p className='pl-3 items-center justify-center'>{text}</p> : null}
+    </>
+
   );
 }
