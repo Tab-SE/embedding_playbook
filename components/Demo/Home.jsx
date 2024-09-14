b91a6eee4743ffc204807e90d113da87f390d3e9
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-09-14 00:33:31 -0600

    first embed

--- components/Demo/Home.jsx
+++ components/Demo/Home.jsx
@@ -1,4 +1,12 @@
-import { Metrics } from 'components';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "components/ui";
+
+import { Metrics, TableauEmbed } from 'components';
 import { Transactions, RecentSales } from './Mockups';
 
 export const description = "An application shell with a header and main content area. The header has a navbar, a search input and and a user nav dropdown. The user nav is toggled by a button with an avatar image. The main content area is divided into two rows. The first row has a grid of cards with statistics. The second row has a grid of cards with a table of recent transactions and a list of recent sales.";
@@ -10,7 +18,22 @@ export const Home = () => {
         <Metrics basis="basis-1/4" />
         <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
           <div className="xl:col-span-2">
-            <Transactions />
+            <Card className='dark:bg-stone-900'>
+              <CardHeader>
+                <CardTitle>Overview</CardTitle>
+                <CardDescription>Your personal digest of Superstore sales in North America</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <TableauEmbed
+                  src='https://prod-useast-b.online.tableau.com/t/embeddingplaybook/views/superstore/overview_800x800'
+                  width={800}
+                  height={1000}
+                  hideTabs={true}
+                  device='default'
+                  toolbar='hidden'
+                />
+              </CardContent>
+            </Card>
           </div>
           <div>
             <RecentSales />
