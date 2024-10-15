09e4a40bb023465fb7cb84dbf559ac43681d1c76
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-10-14 19:22:58 -0600

    changed to _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, lazy, Suspense } from 'react';
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from 'next-themes';
