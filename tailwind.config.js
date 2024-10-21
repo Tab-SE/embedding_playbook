f3b1c4733208c110a0ebe0b631199114714795c4
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-10-21 00:02:52 -0600

    assistant-ui configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,5 +41,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@assistant-ui/react/tailwindcss")({ shadcn: true })
+  ],
 }
