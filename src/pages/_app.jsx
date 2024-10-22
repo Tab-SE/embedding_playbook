886a17872de7e08f21506c82b81243ce2b398fed
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-10-21 20:46:07 -0600

    simplified _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, lazy, Suspense } from 'react';
+import { useState, useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from 'next-themes';
@@ -15,30 +15,10 @@ export default function App({
   // initializes Tanstack Query's client: https://tanstack.com/query/v4/docs/react/reference/QueryClient
   const [queryClient] = useState(new QueryClient());
 
-  const [showDevtools, setShowDevtools] = useState(false)
-
   useEffect(() => {
     // @ts-ignore
     window.toggleDevtools = () => setShowDevtools((old) => !old)
   }, []);
-
-  // const ReactQueryDevtoolsProduction = lazy(async () => {
-  //   const devtools = await import(
-  //     "@tanstack/react-query-devtools/build/modern/production.js"
-  //   );
-
-  //   return {
-  //     default: devtools.ReactQueryDevtools,
-  //   };
-  // });
-
-  const ReactQueryDevtoolsProduction = lazy(() =>
-    import('@tanstack/react-query-devtools/build/modern/production.js').then(
-      (d) => ({
-        default: d.ReactQueryDevtools,
-      }),
-    ),
-  );
 
   // Session Provider: https://next-auth.js.org/getting-started/client#sessionprovider
   // Query Client Provider: https://tanstack.com/query/v4/docs/react/reference/QueryClientProvider
@@ -49,11 +29,6 @@ export default function App({
           <ThemeProvider attribute="class" forcedTheme='light'>
             <AgentRuntimeProvider>
               <Component {...pageProps} />
-              {showDevtools && (
-                <Suspense fallback={null}>
-                  <ReactQueryDevtoolsProduction />
-                </Suspense>
-              )}
               <FloatingAssistant />
             </AgentRuntimeProvider>
           </ThemeProvider>
