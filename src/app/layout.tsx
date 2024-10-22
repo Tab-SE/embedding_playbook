e3d8951c7b499cd95ac8c6206ee01ab767b911e8
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-10-21 21:34:25 -0600

    screen height and width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body>
+      <body className='h-screen w-screen'>
         <SessionProvider>
           <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-left'/>
