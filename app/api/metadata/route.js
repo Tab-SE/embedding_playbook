b436cd27591d315f9318efb05cd99df68de3a9cd
raman-chand <rchand595@gmail.com>
2024-10-29 16:03:18 -0400

    commit maxDuration of 60 for Vercel

--- app/api/metadata/route.js
+++ app/api/metadata/route.js
@@ -1,4 +1,4 @@
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 60; // This function can run for a maximum of 300 seconds
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 import { NextResponse } from "next/server";
