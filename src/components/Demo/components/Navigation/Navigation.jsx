035264ab14915882ccbf7d567a7ad22f12b64740
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-11-25 07:33:39 -0700

    logo large holder

--- src/components/Demo/components/Navigation/Navigation.jsx
+++ src/components/Demo/components/Navigation/Navigation.jsx
@@ -1,14 +1,30 @@
+import Image from 'next/image';
+
 import { NavigationSm, Breadcrumbs, UserMenu } from "components";
+
+import { settings } from '../../settings'
 
 export const Navigation = (props) => {
   const { basePath, crumbs } = props;
 
   return (
-    <header className="sticky top-0 z-30 flex h-20 mt-6 items-center gap-4 border-b bg-background px-4 sm:static sm:border-0 sm:bg-transparent sm:px-6">
-      <NavigationSm />
-      <Breadcrumbs basePath={basePath} crumbs={crumbs} />
-      <div className="relative ml-auto flex-1 md:grow-0"/>
-      <UserMenu />
+    <header >
+      <div className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <NavigationSm />
+        <Image
+          src={settings.logo_large}
+          alt="Logo"
+          width={150}
+          height={34}
+        />
+        <div className="relative ml-auto flex-1 md:grow-0"/>
+        <UserMenu />
+      </div>
+      <Breadcrumbs
+        basePath={basePath}
+        crumbs={crumbs}
+        className={'ml-12 mt-6'}
+      />
     </header>
   )
 }
