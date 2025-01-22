0a32be80a9d99bbb4d64d222f97b3ed3615485b3
Allison Bierschenk <abierschenk@salesforce.com>
2025-01-22 09:36:15 -0600

    new generic embedding playbook to test smtp

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import { DisplayContextProvider } from 'components/context';
 import { HeroDemo } from 'components';
+console.log()
 
 const Intro = () => {
   return (
