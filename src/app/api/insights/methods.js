dc18a45ab6d283899c25004d0cffdc0031069a17
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-02-27 17:24:33 -0700

    updated to use axios wrapper

--- src/app/api/insights/methods.js
+++ src/app/api/insights/methods.js
@@ -1,3 +1,5 @@
+import { httpPost } from 'utils';
+
 const tableau_domain = process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN; // URL for Tableau environment
 const pulse_path = '/api/-/pulse'; // path to resource
 
@@ -23,37 +25,27 @@ export const makePayload = async (rest_key, metric) => {
 
 // requests insight bundles for all supported types given a metric (params)
 const getInsightBundle = async (apiKey, metric, resource) => {
-  // create a request body (standard for all Pulse bundle requests)
   const bundles = makeBundleBody(metric);
-  const body = JSON.stringify(bundles);
+  const endpoint = `${tableau_domain}${pulse_path}/insights${resource}`;
 
-  const endpoint = tableau_domain + pulse_path + '/insights' + resource;
-
-  const request = new Request(endpoint, {
-    method: 'POST',
+  const config = {
     headers: {
       'X-Tableau-Auth': apiKey,
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
-    body: body,
-  });
+  };
 
-  const res = await fetch(request);
-
-  isServerlessTimeout(res);
-
-  const contentType = res.headers.get('content-type');
-
-
-  if (contentType === 'text/html; charset=UTF-8') {
-    const txt = await res.text();
-    throw new Error(txt);
-  } else if (contentType === 'application/json') {
-    const jsonData = await res.json();
-    return jsonData;
+  try {
+    const response = await httpPost(endpoint, bundles, config);
+    return response;
+  } catch (error) {
+    if (error.code === 504) {
+      throw new Error('Serverless Timeout Error:', error);
+    }
+    throw error; // Re-throw other errors
   }
-}
+};
 
 // generetes the complex request body required to generate an insights bundle
 const makeBundleBody = (metric) => {
