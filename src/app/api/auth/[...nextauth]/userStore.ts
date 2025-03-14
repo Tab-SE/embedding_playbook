7e3c1019ac5d5117be791b833e338868c2e2721b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-03-14 11:40:44 -0600

    adding an ebikes route

--- src/app/api/auth/[...nextauth]/userStore.ts
+++ src/app/api/auth/[...nextauth]/userStore.ts
@@ -8,7 +8,7 @@ export const Users = [
         email: "jmorris@mail.com",
         picture: "/img/users/mackenzie_day.png",
         vector_store: 'superstore_jmorris',
-        uaf: {}
+        uaf: {},
       }
     ]
   },
@@ -24,6 +24,43 @@ export const Users = [
         id: 'a',
         name: "Julie Morris",
         email: "jmorris@mail.com",
+        picture: "/img/users/mackenzie_day.png",
+        role: 3,
+        vector_store: 'superstore_jmorris',
+        uaf: {"Region": ["South"]}
+      },
+      {
+        id: 'b',
+        name: "Justin Chen",
+        email: "jchen@mail.com",
+        picture: "/img/users/justin_chen.png",
+        role: 2,
+        vector_store: 'superstore_jchen',
+        uaf: {"Region": ["Central","East"]}
+      },
+      {
+        id: 'c',
+        name: "Sofia Lopez",
+        email: "slopez@mail.com",
+        picture: "/img/users/sofia_lopez.png",
+        role: 1,
+        vector_store: 'superstore_slopez',
+        uaf: {"Region": ["West"]}
+      }
+    ]
+  },
+  {
+    demo: 'ebikes',
+    roles: {
+      1: { title: 'hobby', description: 'Basic data & analytics'},
+      2: { title: 'pro', description: 'More data, analytics & exports'},
+      3: { title: 'premium', description: 'Full self-service analytics'},
+    },
+    users: [
+      {
+        id: 'a',
+        name: "Julie Morris",
+        email: "jmorris@ebikes.com",
         picture: "/img/users/mackenzie_day.png",
         role: 3,
         vector_store: 'superstore_jmorris',
---
+++ src/app/demo/ebikes/customers/page.jsx
@@ -0,0 +1,39 @@
+import { Demo, FloatingAssistant } from 'components';
+import { Customers } from './Customers';
+import { settings } from '../config';
+
+const Page = () => {
+  const { app_name, app_logo, base_path, ai_chat, ai_avatar, sections } = settings;
+
+  // for the most part, only the pageName and child components for <Demo/> should be modified to make new pages
+  const pageName = 'Customers';
+
+  return (
+    <Demo
+      app_name={app_name}
+      base_path={base_path}
+      crumbs={{
+        [app_name]: {
+          path: '/',
+          child: {
+            [pageName]: {
+              path: `${pageName ? pageName.toLowerCase() : ''}`,
+              child: null
+            }
+          }
+        }
+      }}
+      app_logo={app_logo}
+      ai_chat={ai_chat}
+      ai_avatar={ai_avatar}
+      sections={sections}
+    >
+      <Customers />
+      <FloatingAssistant
+        ai_avatar={ai_avatar}
+      />
+    </Demo>
+  )
+}
+
+export default Page;
