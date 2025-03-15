b307857c93f808aec2cb6b807cc6f37932162f35
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-03-14 22:25:16 -0600

    fixed Gallery from nested <a> tags

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import Link from 'next/link'
 
@@ -21,45 +23,41 @@ export const Gallery = (props) => {
       </header>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
         {galleryItems.map((item) => (
-            <HoverCard key={item.id}>
-              <HoverCardTrigger>
-                <Link
-                  href={item.link}
-                >
-                  <Card
-                    className="overflow-hidden shadow-2xl h-60 transform transition-transform duration-300 hover:scale-110 cursor-pointer"
-                  >
-                    <CardContent className="p-0">
-                      <Image
-                        src={item.src}
-                        alt={item.alt}
-                        width={318}
-                        height={171}
-                        className="w-full object-cover"
-                      />
-                    </CardContent>
-                  </Card>
-                </Link>
-              </HoverCardTrigger>
-              <HoverCardContent className="w-96 shadow-2xl" sideOffset={15}>
-                <div className="flex justify-between space-x-3">
-                  <div className="space-y-1">
-                    <h4 className="text-sm font-semibold">{item.alt}</h4>
-                    <p className="text-sm">
-                      {item.description}
-                    </p>
-                    <div className="flex items-center pt-2">
-                      <Badge>
-                        {item.icon}
-                        <span className="text-xs text-muted-foreground ml-1">
-                          {item.vertical}
-                        </span>
-                      </Badge>
-                    </div>
+          <HoverCard key={item.id}>
+            <HoverCardTrigger asChild>
+              <Link href={item.link}>
+                <Card className="overflow-hidden shadow-2xl h-60 transform transition-transform duration-300 hover:scale-110 cursor-pointer">
+                  <CardContent className="p-0">
+                    <Image
+                      src={item.src}
+                      alt={item.alt}
+                      width={318}
+                      height={171}
+                      className="w-full object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </Link>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-96 shadow-2xl" sideOffset={15}>
+              <div className="flex justify-between space-x-3">
+                <div className="space-y-1">
+                  <h4 className="text-sm font-semibold">{item.alt}</h4>
+                  <p className="text-sm">
+                    {item.description}
+                  </p>
+                  <div className="flex items-center pt-2">
+                    <Badge>
+                      {item.icon}
+                      <span className="text-xs text-muted-foreground ml-1">
+                        {item.vertical}
+                      </span>
+                    </Badge>
                   </div>
                 </div>
-              </HoverCardContent>
-            </HoverCard>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
         ))}
       </div>
     </div>
