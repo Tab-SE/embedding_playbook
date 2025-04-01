532d02c9c6af302c17312353d18d06cb149c3140
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-04-01 00:19:59 -0600

    removed page name

--- src/app/demo/superstore/page.jsx
+++ src/app/demo/superstore/page.jsx
@@ -5,7 +5,7 @@ import { settings } from './config';
 
 const Page = () => {
   // for the most part, only the pageName and child components for <Demo/> should be modified to make new pages
-  const pageName = 'Home';
+  const pageName = '';
 
   return (
     <Demo
