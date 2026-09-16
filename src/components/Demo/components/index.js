be9f354d5e40d1ac7ce93164458b5ddefd272557
allison bierschenk <allisonreynoldsc@gmail.com>
2026-09-16 09:11:39 -0500

    fix thumbnail

--- src/components/Demo/components/index.js
+++ src/components/Demo/components/index.js
@@ -1,3 +1,4 @@
 export { Demo } from './Demo';
+export { DemoErrorBoundary } from './DemoErrorBoundary';
 export { NavigationMd, NavigationSm, Breadcrumbs, UserMenu, Navigation } from './Navigation';
 export { Auth } from './Auth';
